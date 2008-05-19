f1e89b2bb9f7d2677cd589f808df9594987fb1e0
Richard Purdie <richard@openedhand.com>
2008-05-19 20:41:58 +0000

    bitbake: Sync with upstream
    git-svn-id: https://svn.o-hand.com/repos/poky/trunk@4504 311d38ba-8fff-0310-9ca6-ca027cbcb966

--- bitbake/lib/bb/event.py
+++ bitbake/lib/bb/event.py
@@ -230,6 +230,10 @@ class BuildStarted(BuildBase):
     """bbmake build run started"""
 
 
+class BuildFailed(BuildBase):
+    """bbmake build run failed"""
+
+
 class BuildCompleted(BuildBase):
     """bbmake build run completed"""
 
