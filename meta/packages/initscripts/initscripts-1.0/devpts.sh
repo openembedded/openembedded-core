6cd426013e1701845a03a42bfd45a45cc3f13c91
Ross Burton <ross@openedhand.com>
2008-06-25 15:09:05 +0000

    initscripts: don't create subshells for simple tests
    git-svn-id: https://svn.o-hand.com/repos/poky/trunk@4726 311d38ba-8fff-0310-9ca6-ca027cbcb966

--- meta/packages/initscripts/initscripts-1.0/devpts.sh
+++ meta/packages/initscripts/initscripts-1.0/devpts.sh
@@ -1,14 +1,16 @@
 #! /bin/sh
+### BEGIN INIT INFO
+# Provides:          devpts
+# Required-Start: udev
+# Required-Stop:
+# Default-Start:     S
+# Default-Stop:
+# Short-Description: Mount /dev/pts file systems.
+### END INIT INFO
 
 . /etc/default/devpts
 
-test "`uname -s`" = "Linux" || exit 0
-
-#
-#	First find out if devpts is available. Also check if devfs
-#	is already mounted - in that case we don't want to use devpts.
-#
-if test ! -e /dev/.devfsd && ( grep -q devpts /proc/filesystems )
+if grep -q devpts /proc/filesystems
 then
 	#
 	#	Create multiplexor device.
@@ -18,11 +20,9 @@ then
 	#
 	#	Mount /dev/pts if needed.
 	#
-	if ( ! grep -q devpts /proc/mounts )
+	if ! grep -q devpts /proc/mounts
 	then
 		mkdir -p /dev/pts
 		mount -t devpts devpts /dev/pts -ogid=${TTYGRP},mode=${TTYMODE}
 	fi
 fi
-
-exit 0
