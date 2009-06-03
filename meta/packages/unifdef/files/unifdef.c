0223f45de06c35c4f83e4f3a09e96a84f916f99d
Richard Purdie <richard@ted.(none)>
2009-06-03 18:27:30 +0100

    unifdef: rename getline function to avoid conflicts with stdio.h definitions from glibc
    Signed-off-by: Richard Purdie <richard@ted.(none)>

--- meta/packages/unifdef/files/unifdef.c
+++ meta/packages/unifdef/files/unifdef.c
@@ -206,7 +206,7 @@ static void             done(void);
 static void             error(const char *);
 static int              findsym(const char *);
 static void             flushline(bool);
-static Linetype         getline(void);
+static Linetype         getline34(void);
 static Linetype         ifeval(const char **);
 static void             ignoreoff(void);
 static void             ignoreon(void);
@@ -512,7 +512,7 @@ process(void)
 
 	for (;;) {
 		linenum++;
-		lineval = getline();
+		lineval = getline34();
 		trans_table[ifstate[depth]][lineval]();
 		debug("process %s -> %s depth %d",
 		    linetype_name[lineval],
@@ -526,7 +526,7 @@ process(void)
  * help from skipcomment().
  */
 static Linetype
-getline(void)
+getline34(void)
 {
 	const char *cp;
 	int cursym;
