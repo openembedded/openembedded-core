71a46a44a2f278db1ac1b309ff52b7836f3286b4
Richard Purdie <rpurdie@linux.intel.com>
2010-08-23 13:01:05 +0100

    utils.py: Fix bb.copyfile to change the permissions of the file back correctly
    Signed-off-by: Richard Purdie <rpurdie@linux.intel.com>

--- bitbake/lib/bb/utils.py
+++ bitbake/lib/bb/utils.py
@@ -703,9 +703,11 @@ def copyfile(src, dest, newmtime = None, sstat = None):
             os.rename(dest + "#new", dest)
         except Exception as e:
             print('copyfile: copy', src, '->', dest, 'failed.', e)
-            os.chmod(src, stat.S_IMODE(sstat[stat.ST_MODE]))
-            os.utime(src, (sstat[stat.ST_ATIME], sstat[stat.ST_MTIME]))
             return False
+        finally:
+            os.chmod(src, sstat[stat.ST_MODE])
+            os.utime(src, (sstat[stat.ST_ATIME], sstat[stat.ST_MTIME]))
+
     else:
         #we don't yet handle special, so we need to fall back to /bin/mv
         a = getstatusoutput("/bin/cp -f " + "'" + src + "' '" + dest + "'")
