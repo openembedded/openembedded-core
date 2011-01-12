3f08b7f1eadc8d181c76339d14c7abb4f49cad57
Richard Purdie <richard.purdie@linuxfoundation.org>
2011-01-12 13:45:39 +0000

    bitbake/knotty.py: Ensure task note messages are only surpressed at default log levels
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- bitbake/lib/bb/ui/knotty.py
+++ bitbake/lib/bb/ui/knotty.py
@@ -113,8 +113,12 @@ def main(server, eventHandler):
             if isinstance(event, logging.LogRecord):
                 if event.levelno >= format.ERROR:
                     return_value = 1
-                if event.taskpid != 0 and event.levelno <= format.NOTE:
-                    continue
+                # For "normal" logging conditions, don't show note logs from tasks
+                # but do show them if the user has changed the default log level to 
+                # include verbose/debug messages
+                if logger.getEffectiveLevel() > format.VERBOSE:
+                    if event.taskpid != 0 and event.levelno <= format.NOTE:
+                        continue
                 logger.handle(event)
                 continue
 
