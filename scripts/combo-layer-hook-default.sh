68394476748386e58f40173643967f5a248173b1
Yu Ke <ke.yu@intel.com>
2011-06-13 20:20:53 +0800

    combo-layer-tool: add tool to manipulate combo layers
    This patch adds the script "combo-layer" to manipulate combo layer
    repos. A combo layer repo is a repo containing multiple component
    repos, e.g. oe-core, bitbake, BSP repos. The combo layer repo needs to
    be updated by syncing with the component repo upstream. This script
    is written to assist the combo layer handling.
    
    The combo layer tool provides three functionalities:
    - init: when the combo layer repo and component repo does not exist,
      init will "git init" the combo layer repo, and also "git clone" the
      component repos
    
    - update: combo layer tool will pull the latest commit from component
      repo upstream, and apply the commits since last update commit to the
      combo repo. If the user specifies interactive mode(--interactive),
      they can edit the patch list to select which commits to apply.
    
    - splitpatch: split the combo repo commit into separate patches per
      component repo, to facilitate upstream submission.
    
    Combo layer tool uses a config file to define the component repo info.
    Please check the combo-layer.conf.example for a detailed explanation
    of the config file fields.
    
    Signed-off-by: Yu Ke <ke.yu@intel.com>
    Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>

---
+++ scripts/combo-layer-hook-default.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Hook to add source component/revision info to commit message
+# Parameter:
+#   $1 patch-file
+#   $2 revision
+#   $3 reponame
+
+patchfile=$1
+rev=$2
+reponame=$3
+
+sed -i -e "s#Subject: \[PATCH\] \(.*\)#Subject: \[PATCH\] $reponame: \1#" $patchfile
+sed -i -e "0,/Signed-off-by:/s#\(Signed-off-by:.*\)#\($reponame rev: $rev\)\n\n\1#" $patchfile
