e7aae45414e4597e9244f86a81fbc940f73785c8
Paul Eggleton <paul.eggleton@linux.intel.com>
2011-08-23 19:56:28 +0100

    scripts/combo-layer: fix overzealous regex in default hook script
    combo-layer's hook scripts are intended to modify patches as they pass
    through; the default one adds a prefix with the component name and
    a line with the component repo revision before the Signed-off-by;
    however the script was also unintentionally modifying the contents of
    patches *within* the patches passing through it, which resulted in
    unexpected failures when the combo-layer script attempted to use
    "git am" to apply them.
    
    Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>

--- scripts/combo-layer-hook-default.sh
+++ scripts/combo-layer-hook-default.sh
@@ -9,5 +9,5 @@ patchfile=$1
 rev=$2
 reponame=$3
 
-sed -i -e "s#Subject: \[PATCH\] \(.*\)#Subject: \[PATCH\] $reponame: \1#" $patchfile
-sed -i -e "0,/Signed-off-by:/s#\(Signed-off-by:.*\)#\($reponame rev: $rev\)\n\n\1#" $patchfile
+sed -i -e "s#^Subject: \[PATCH\] \(.*\)#Subject: \[PATCH\] $reponame: \1#" $patchfile
+sed -i -e "0,/^Signed-off-by:/s#\(Signed-off-by:.*\)#\($reponame rev: $rev\)\n\n\1#" $patchfile
