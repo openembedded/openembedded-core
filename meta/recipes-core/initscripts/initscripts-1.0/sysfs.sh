fdb3d74458cd36a0fe53ee097394acc37d658ce9
Otavio Salvador <otavio@ossystems.com.br>
2011-12-30 15:58:47 +0000

    initscripts: avoid mounting /sys if it is already mounted
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- meta/recipes-core/initscripts/initscripts-1.0/sysfs.sh
+++ meta/recipes-core/initscripts/initscripts-1.0/sysfs.sh
@@ -14,6 +14,6 @@ if [ -e /proc ] && ! [ -e /proc/mounts ]; then
   mount -t proc proc /proc
 fi
 
-if [ -e /sys ] && grep -q sysfs /proc/filesystems; then
-  mount sysfs /sys -t sysfs
+if [ -e /sys ] && grep -q sysfs /proc/filesystems && ! [ -e /sys/class ]; then
+  mount -t sysfs sysfs /sys
 fi
