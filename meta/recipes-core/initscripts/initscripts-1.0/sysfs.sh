55c4d3c55e4c3a7c2cda6d006cf7b78567bd3298
Tom Zanussi <tom.zanussi@intel.com>
2012-09-21 14:11:45 -0500

    initscripts/sysfs.sh: mount debugfs if present
    debugfs is another kernel virtual file system that should be mounted
    if configured, so if it's configured into the kernel, mount it.
    
    Signed-off-by: Tom Zanussi <tom.zanussi@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/recipes-core/initscripts/initscripts-1.0/sysfs.sh
+++ meta/recipes-core/initscripts/initscripts-1.0/sysfs.sh
@@ -17,3 +17,7 @@ fi
 if [ -e /sys ] && grep -q sysfs /proc/filesystems && ! [ -e /sys/class ]; then
   mount -t sysfs sysfs /sys
 fi
+
+if [ -e /sys/kernel/debug ] && grep -q debugfs /proc/filesystems; then
+  mount -t debugfs debugfs /sys/kernel/debug
+fi
