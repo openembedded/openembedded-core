ostroproject/ostro-os

pulseaudio: explicitly disable xen, rather than letting it detect
(From OE-core rev: d109b24ad354382cd40b28e86211e53929a0910f)

Signed-off-by: Gilbert Coville <gilbert_coville@mentor.com>
Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

meta/recipes-multimedia/pulseaudio/pulseaudio_2.1.bb
@@ -1,6 +1,6 @@
 require pulseaudio.inc
 
-PR = "r12"
+PR = "r14"
 
 DEPENDS += "libjson gdbm speex libxml-parser-perl-native"
 

openembedded/openembedded-core

meta/recipes-multimedia/pulseaudio/pulseaudio_2.1.bb
@@ -1,6 +1,6 @@
 require pulseaudio.inc
 
-PR = "r7"
+PR = "r8"
 
 DEPENDS += "libjson gdbm speex libxml-parser-perl-native"
 