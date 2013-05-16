20ded0315924a083e07900a8a3308bd351b15a8b
Ross Burton <ross.burton@intel.com>
2013-05-16 13:48:42 +0000

    Revert "initramfs-live-install*: fix the "install" boot option"
    This patch causes install to fail because /etc/mtab doesn't exist, so revert it
    for now until the root problem is fixed.
    
    [ YOCTO #4501 ]
    
    This reverts commit 2f5d41f94c53aaf606de66891c37de0dbd86f54b.
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/recipes-core/initrdscripts/files/init-install-efi.sh
+++ meta/recipes-core/initrdscripts/files/init-install-efi.sh
@@ -66,6 +66,7 @@ rm -f /etc/udev/scripts/mount*
 umount /dev/${device}* 2> /dev/null || /bin/true
 
 mkdir -p /tmp
+cat /proc/mounts > /etc/mtab
 
 disk_size=$(parted /dev/${device} unit mb print | grep Disk | cut -d" " -f 3 | sed -e "s/MB//")
 
