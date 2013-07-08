c072fed6531f2ce3c687f8342a97f593ebf37653
Stefan Stanacar <stefanx.stanacar@intel.com>
2013-07-08 16:40:00 +0300

    lib/oeqa/runtime/dmesg.py: whitelist dmesg message for systemd net interface name
    Messages such as:
    "systemd-udevd[79]: error changing net interface name eth0 to enp0s3: Device or resource busy"
    are harmless as systemd can't rename interfaces in qemu (the interface is already active).
    Alternatively, passing net.ifnames=0 to the kernel will stop systemd renaming the interfaces.
    
    Signed-off-by: Stefan Stanacar <stefanx.stanacar@intel.com>
    Signed-off-by: Saul Wold <sgw@linux.intel.com>

--- meta/lib/oeqa/runtime/dmesg.py
+++ meta/lib/oeqa/runtime/dmesg.py
@@ -9,5 +9,5 @@ class DmesgTest(oeRuntimeTest):
 
     @skipUnlessPassed('test_ssh')
     def test_dmesg(self):
-        (status, output) = self.target.run('dmesg | grep -v mmci-pl18x | grep -i error')
+        (status, output) = self.target.run('dmesg | grep -v mmci-pl18x | grep -v "error changing net interface name" | grep -i error')
         self.assertEqual(status, 1, msg = "Error messages in dmesg log: %s" % output)
