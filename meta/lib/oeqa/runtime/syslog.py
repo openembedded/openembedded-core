c75f3e2385dde44ee96e33f4e5d064894dfb7d52
Stefan Stanacar <stefanx.stanacar@intel.com>
2013-08-09 16:26:40 +0300

    lib/oeqa/runtime: rework syslog test
    Add separate tests for restarting syslog and using logger, and
    skip the configuration test for systemd images which always fail
    because syslog's systemd service doesn't read a config by default
    (see YB#4860).
    
    Signed-off-by: Stefan Stanacar <stefanx.stanacar@intel.com>
    Signed-off-by: Saul Wold <sgw@linux.intel.com>

--- meta/lib/oeqa/runtime/syslog.py
+++ meta/lib/oeqa/runtime/syslog.py
@@ -14,24 +14,33 @@ class SyslogTest(oeRuntimeTest):
         self.assertEqual(status, 1, msg="status and output: %s and %s" % (status,output))
 
     @skipUnlessPassed("test_syslog_help")
-    def test_syslogd_running(self):
+    def test_syslog_running(self):
         (status,output) = self.target.run(oeRuntimeTest.pscmd + ' | grep -i [s]yslogd')
         self.assertEqual(status, 0, msg="no syslogd process, ps output: %s" % self.target.run(oeRuntimeTest.pscmd)[1])
 
+
 class SyslogTestConfig(oeRuntimeTest):
 
-    def setUp(self):
-        self.target.run('echo "LOGFILE=/var/log/test" >> /etc/syslog-startup.conf')
+    @skipUnlessPassed("test_syslog_running")
+    def test_syslog_logger(self):
+        (status,output) = self.target.run('logger foobar && grep foobar /var/log/messages')
+        self.assertEqual(status, 0, msg="Test log string not found in /var/log/messages. Output: %s " % output)
 
-    @skipUnlessPassed("test_syslogd_running")
-    def test_syslogd_configurable(self):
+    @skipUnlessPassed("test_syslog_running")
+    def test_syslog_restart(self):
         if "systemd" != oeRuntimeTest.tc.d.getVar("VIRTUAL-RUNTIME_init_manager"):
             (status,output) = self.target.run('/etc/init.d/syslog restart')
         else:
             (status,output) = self.target.run('systemctl restart syslog.service')
+
+    @skipUnlessPassed("test_syslog_restart")
+    @skipUnlessPassed("test_syslog_logger")
+    @unittest.skipIf("systemd" == oeRuntimeTest.tc.d.getVar("VIRTUAL-RUNTIME_init_manager"), "Not appropiate for systemd image")
+    def test_syslog_startup_config(self):
+        self.target.run('echo "LOGFILE=/var/log/test" >> /etc/syslog-startup.conf')
+        (status,output) = self.target.run('/etc/init.d/syslog restart')
         self.assertEqual(status, 0, msg="Could not restart syslog service. Status and output: %s and %s" % (status,output))
         (status,output) = self.target.run('logger foobar && grep foobar /var/log/test')
         self.assertEqual(status, 0, msg="Test log string not found. Output: %s " % output)
-
-    def tearDown(self):
         self.target.run("sed -i 's#LOGFILE=/var/log/test##' /etc/syslog-startup.conf")
+        self.target.run('/etc/init.d/syslog restart')
