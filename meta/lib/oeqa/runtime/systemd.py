6386dc718f85210c9b6b9f69878ec9a7847b78de
Alexandru Palalau <alexandrux.palalau@intel.com>
2013-08-09 10:59:12 +0300

    lib/oeqa/runtime: add new systemd tests
    New systemd runtime tests for enable/disable service, start/stop service and list services.
    
    Signed-off-by: Alexandru Palalau <alexandrux.palalau@intel.com>
    Signed-off-by: Stefan Stanacar <stefanx.stanacar@intel.com>
    Signed-off-by: Saul Wold <sgw@linux.intel.com>

--- meta/lib/oeqa/runtime/systemd.py
+++ meta/lib/oeqa/runtime/systemd.py
@@ -22,3 +22,38 @@ class SystemdTests(oeRuntimeTest):
     def test_systemd_failed(self):
         (status, output) = self.target.run('systemctl --failed | grep "0 loaded units listed"')
         self.assertEqual(status, 0, msg="Failed systemd services: %s" % self.target.run('systemctl --failed')[1])
+
+    @skipUnlessPassed('test_systemd_version')
+    def test_systemd_service(self):
+        (status, output) = self.target.run('systemctl list-unit-files | grep "systemd-hostnamed.service"')
+        self.assertEqual(status, 0, msg="systemd-hostnamed.service service is not available.")
+
+    @skipUnlessPassed('test_systemd_service')
+    def test_systemd_stop(self):
+        self.target.run('systemctl stop systemd-hostnamed.service')
+        (status, output) = self.target.run('systemctl show systemd-hostnamed.service | grep "ActiveState" | grep "=inactive"')
+        self.assertEqual(status, 0, msg="systemd-hostnamed.service service could not be stopped.Status and output: %s and %s" % (status, output))
+
+    @skipUnlessPassed('test_systemd_stop')
+    @skipUnlessPassed('test_systemd_version')
+    def test_systemd_start(self):
+        self.target.run('systemctl start systemd-hostnamed.service')
+        (status, output) = self.target.run('systemctl show systemd-hostnamed.service | grep "ActiveState" | grep "=active"')
+        self.assertEqual(status, 0, msg="systemd-hostnamed.service service could not be started. Status and output: %s and %s" % (status, output))
+
+    @skipUnlessPassed('test_systemd_version')
+    def test_systemd_enable(self):
+        self.target.run('systemctl enable machineid.service')
+        (status, output) = self.target.run('systemctl is-enabled machineid.service')
+        self.assertEqual(output, 'enabled', msg="machineid.service service could not be enabled. Status and output: %s and %s" % (status, output))
+
+    @skipUnlessPassed('test_systemd_enable')
+    def test_systemd_disable(self):
+        self.target.run('systemctl disable machineid.service')
+        (status, output) = self.target.run('systemctl is-enabled machineid.service')
+        self.assertEqual(output, 'disabled', msg="machineid.service service could not be disabled. Status and output: %s and %s" % (status, output))
+
+    @skipUnlessPassed('test_systemd_version')
+    def test_systemd_list(self):
+        (status, output) = self.target.run('systemctl list-unit-files')
+        self.assertEqual(status, 0, msg="systemctl list-unit-files command failed. Status:  %s" % status)
