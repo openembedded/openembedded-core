8b90ed084d59b4e07aa547255d327b25cfb2ee2b
Richard Purdie <richard.purdie@linuxfoundation.org>
2013-11-08 15:19:05 +0000

    lib/oe/path: Fix performance issue got copyhardlinktree()
    With the directory copy was added to avoid race issues, it wasn't noticed that
    tar was recursing the directories and copying files too. This is completely
    crazy when we hardlink those files in the next command.
    
    Resolve the issue by telling tar not to recurse. This gives a significant
    performance boost to various parts of the system (do_package for linux-yocto
    256s -> 178s for example).
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oe/path.py
+++ meta/lib/oe/path.py
@@ -93,7 +93,7 @@ def copyhardlinktree(src, dst):
     if (os.stat(src).st_dev ==  os.stat(dst).st_dev):
         # Need to copy directories only with tar first since cp will error if two 
         # writers try and create a directory at the same time
-        cmd = 'cd %s; find . -type d -print | tar -cf - -C %s -p --files-from - | tar -xf - -C %s' % (src, src, dst)
+        cmd = 'cd %s; find . -type d -print | tar -cf - -C %s -p --files-from - --no-recursion | tar -xf - -C %s' % (src, src, dst)
         check_output(cmd, shell=True, stderr=subprocess.STDOUT)
         if os.path.isdir(src):
             src = src + "/*"
