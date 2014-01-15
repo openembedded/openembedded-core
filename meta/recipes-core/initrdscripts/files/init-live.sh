5a2f4feeaac4f9278fa0cf808c2f495f0c19324f
Krzysztof Sywula <krzysztof.m.sywula@intel.com>
2014-01-15 11:27:04 +0000

    initrdscripts: add $CMDLINE to init-live switch_root
    init-live.sh: $CMDLINE variable should be provided to switch_root
    to let user specify runlevel on grub command line.
    Feeding with -c /dev/console as well as busybox switch_root enables that option.
    
    Signed-off-by: Krzysztof Sywula <krzysztof.m.sywula@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/recipes-core/initrdscripts/files/init-live.sh
+++ meta/recipes-core/initrdscripts/files/init-live.sh
@@ -89,7 +89,10 @@ boot_live_root() {
     mount -n --move /dev ${ROOT_MOUNT}/dev
 
     cd $ROOT_MOUNT
-    exec switch_root $ROOT_MOUNT /sbin/init
+
+    # busybox switch_root supports -c option
+    exec switch_root -c /dev/console $ROOT_MOUNT /sbin/init $CMDLINE ||
+        fatal "Couldn't switch_root, dropping to shell"
 }
 
 fatal() {
