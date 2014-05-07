f1f97c61281158432a98279a2f7b4931173f406e
Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
2014-05-07 13:23:35 +0200

    package_manager: Fix NoneType Object on do_populate_sdk
    PACKAGE_EXCLUDE can be not defined or empty, leading to a build error.
    
    File: '/var/lib/jenkins/jobs/qt5022-cesium/workspace/repo/yocto/meta/lib/oe/package_manager.py', lineno: 1649, function: _create_configs
         1645:                    "Pin-Priority: %d\n\n" % (arch, priority))
         1646:
         1647:                priority += 5
         1648:
     *** 1649:            for pkg in self.d.getVar('PACKAGE_EXCLUDE', True).split():
         1650:                prefs_file.write(
         1651:                    "Package: %s\n"
         1652:                    "Pin: release *\n"
         1653:                    "Pin-Priority: -1\n\n" % pkg)
    Exception: AttributeError: 'NoneType' object has no attribute 'split'
    
    ERROR: Function failed: do_populate_sdk
    ERROR: Logfile of failure stored in: /var/lib/jenkins/jobs/qt5022-cesium/workspace/build/tmp/work/bobcat_64-poky-linux/meta-toolchain/1.0-r7/temp/log.do_populate_sdk.21363
    NOTE: recipe meta-toolchain-1.0-r7: task do_populate_sdk: Failed
    (From OE-Core rev: 26314886c3712f980ccc589b014a8f1802193b56)
    
    Signed-off-by: Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
    Signed-off-by: Saul Wold <sgw@linux.intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oe/package_manager.py
+++ meta/lib/oe/package_manager.py
@@ -1644,7 +1644,8 @@ class DpkgPM(PackageManager):
 
                 priority += 5
 
-            for pkg in self.d.getVar('PACKAGE_EXCLUDE', True).split():
+            pkg_exclude = self.d.getVar('PACKAGE_EXCLUDE', True) or ""
+            for pkg in pkg_exclude:
                 prefs_file.write(
                     "Package: %s\n"
                     "Pin: release *\n"
