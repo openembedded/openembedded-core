c598dbdbaebf95cc26e95138b4c3fcb15af67a88
Richard Purdie <richard.purdie@linuxfoundation.org>
2014-10-02 10:56:59 +0100

    oeqa/dmesg: Whitelist usbhid failures
    We fixed these in parselog but not here. This test really can just be
    deleted now really.
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oeqa/runtime/dmesg.py
+++ meta/lib/oeqa/runtime/dmesg.py
@@ -8,5 +8,5 @@ class DmesgTest(oeRuntimeTest):
     @testcase(215)
     @skipUnlessPassed('test_ssh')
     def test_dmesg(self):
-        (status, output) = self.target.run('dmesg | grep -v mmci-pl18x | grep -v "error changing net interface name" | grep -iv "dma timeout" | grep -i error')
+        (status, output) = self.target.run('dmesg | grep -v mmci-pl18x | grep -v "error changing net interface name" | grep -iv "dma timeout" | grep -v usbhid | grep -i error')
         self.assertEqual(status, 1, msg = "Error messages in dmesg log: %s" % output)
