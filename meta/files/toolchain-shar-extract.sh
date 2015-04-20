617225af7a7cc9cb038fb1e6466af03867482032
Paul Eggleton <paul.eggleton@linux.intel.com>
2015-04-20 17:47:09 +0100

    toolchain-shar-extract.sh: mention how to set up the SDK environment
    Tell the user how to set up the SDK environment each time they want to
    use it.
    
    Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/files/toolchain-shar-extract.sh
+++ meta/files/toolchain-shar-extract.sh
@@ -151,6 +151,8 @@ if [ $savescripts = 0 ] ; then
 fi
 
 echo "SDK has been successfully set up and is ready to be used."
+echo "Each time you wish to use the SDK in a new shell session, you need to source the environment setup script e.g."
+echo " \$ . $target_sdk_dir/environment-setup-@REAL_MULTIMACH_TARGET_SYS@"
 
 exit 0
 
