66c6d0db27c4ab7b633e6954d095c411a98b67f4
Alejandro Hernandez <alejandro.hernandez@linux.intel.com>
2015-05-14 16:13:40 +0000

    python3: fix pyconfig.h installation on target
    The file pyconfig.h wasnt being deployed on target, causing an error
    when importing some libraries that required it, this patch fixes
    python3 manifest to include this file and fix the issue.
    
    [YOCTO #7764]
    
    Signed-off-by: Alejandro Hernandez <alejandro.hernandez@linux.intel.com>

--- scripts/contrib/python/generate-manifest-3.4.py
+++ scripts/contrib/python/generate-manifest-3.4.py
@@ -175,7 +175,7 @@ if __name__ == "__main__":
     "lib-dynload/binascii.*.so lib-dynload/_struct.*.so lib-dynload/time.*.so " +
     "lib-dynload/xreadlines.*.so types.* platform.* ${bindir}/python* "  + 
     "_weakrefset.* sysconfig.* _sysconfigdata.* config/Makefile " +
-    "${includedir}/python${PYTHON_MAJMIN}/pyconfig*.h " +
+    "${includedir}/python${PYTHON_BINABI}/pyconfig*.h " +
     "${libdir}/python${PYTHON_MAJMIN}/collections " +
     "${libdir}/python${PYTHON_MAJMIN}/_collections_abc.* " +
     "${libdir}/python${PYTHON_MAJMIN}/_sitebuiltins.* " +
