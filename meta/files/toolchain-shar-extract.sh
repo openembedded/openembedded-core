7511862faad1c28804e2410ff42747c8706c5207
Qi.Chen@windriver.com <Qi.Chen@windriver.com>
2015-09-07 13:42:25 +0100

    Extensible SDK: allow for installation without preparing build system
    When publishing SDK, what we want is basically its metadata and sstate
    cache objects. We don't want the SDK to be prepared with running bitbake
    as it takes time which reproduces meaningless output for the published SDK.
    
    So this patch adds an option to allow for SDK to be extracted without
    preparing the build system.
    
    Signed-off-by: Chen Qi <Qi.Chen@windriver.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/files/toolchain-shar-extract.sh
+++ meta/files/toolchain-shar-extract.sh
@@ -32,13 +32,16 @@ answer=""
 relocate=1
 savescripts=0
 verbose=0
-while getopts ":yd:DRS" OPT; do
+while getopts ":yd:nDRS" OPT; do
 	case $OPT in
 	y)
 		answer="Y"
 		;;
 	d)
 		target_sdk_dir=$OPTARG
+		;;
+	n)
+		prepare_buildsystem="no"
 		;;
 	D)
 		verbose=1
@@ -54,6 +57,8 @@ while getopts ":yd:DRS" OPT; do
 		echo "Usage: $(basename $0) [-y] [-d <dir>]"
 		echo "  -y         Automatic yes to all prompts"
 		echo "  -d <dir>   Install the SDK to <dir>"
+		echo "======== Extensible SDK only options ============"
+		echo "  -n         Do not prepare the build system"
 		echo "======== Advanced DEBUGGING ONLY OPTIONS ========"
 		echo "  -S         Save relocation scripts"
 		echo "  -R         Do not relocate executables"
