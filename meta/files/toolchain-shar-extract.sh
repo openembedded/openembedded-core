0129a12dd3bdb0e9966643c3a355d5eec846da8b
Brendan Le Foll <brendan.le.foll@intel.com>
2015-09-07 13:42:19 +0100

    toolchain-shar-extract.sh: explain why we cannot use sudo in extensible SDK
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/files/toolchain-shar-extract.sh
+++ meta/files/toolchain-shar-extract.sh
@@ -136,7 +136,8 @@ mkdir -p $target_sdk_dir >/dev/null 2>&1
 # if don't have the right to access dir, gain by sudo 
 if [ ! -x $target_sdk_dir -o ! -w $target_sdk_dir -o ! -r $target_sdk_dir ]; then 
 	if [ "$SDK_EXTENSIBLE" = "1" ]; then
-		echo "Unable to access \"$target_sdk_dir\"."
+		echo "Unable to access \"$target_sdk_dir\", will not attempt to use" \
+		     "sudo as as extensible SDK cannot be used as root."
 		exit 1
 	fi
 
