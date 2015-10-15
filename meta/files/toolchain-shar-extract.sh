b388468deda8d81df80d1509226196a5390183a8
Maxin B. John <maxin.john@intel.com>
2015-10-15 10:00:40 +0300

    toolchain-shar-extract.sh: provide proper path for env_setup_script
    The toolchain install script suggest the user to source env_setup_script
    from wrong path now. eg:
    
    " Each time you wish to use the SDK in a new shell session, you need to
    source the environment setup script e.g.
    $ . /opt/poky/2.0//opt/poky/2.0/environment-setup-armv5e-poky-linux-gnueabi
    "
    
    fix it.
    
    Signed-off-by: Maxin B. John <maxin.john@intel.com>
    Signed-off-by: Ross Burton <ross.burton@intel.com>

--- meta/files/toolchain-shar-extract.sh
+++ meta/files/toolchain-shar-extract.sh
@@ -183,7 +183,7 @@ fi
 echo "SDK has been successfully set up and is ready to be used."
 echo "Each time you wish to use the SDK in a new shell session, you need to source the environment setup script e.g."
 for env_setup_script in `ls $target_sdk_dir/environment-setup-*`; do
-	echo " \$ . $target_sdk_dir/$env_setup_script"
+	echo " \$ . $env_setup_script"
 done
 
 exit 0
