4d879cb8d7384ac4a96f22c1664b8875f2d8f615
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2015-12-03 15:54:25 +0200

    devtool: extract: update SRCTREECOVEREDTASKS for kernel
    Add 'do_kernel_configme' and 'do_kernel_configcheck' to
    SRCTREECOVEREDTASKS of kernel packages. These tasks should not be run
    because kernel meta in the srctree is not necessarily up-to-date or
    even present which causes build failures and/or invalid kernel config.
    Especially so because 'do_patch' which is a dependency of
    'do_kernel_configme' is not being run.
    
    We now store .config in the srctree and 'do_configure' task is able to
    run successfully.
    
    (From OE-Core master rev: 7ce4c18a4ba1ebcb9f46e652a881ace1f21d2292)
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>
    Signed-off-by: Ross Burton <ross.burton@intel.com>
    Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- scripts/lib/devtool/standard.py
+++ scripts/lib/devtool/standard.py
@@ -599,7 +599,8 @@ def modify(args, config, basepath, workspace):
             f.write('EXTERNALSRC_BUILD_pn-%s = "%s"\n' % (pn, srctree))
 
         if bb.data.inherits_class('kernel', rd):
-            f.write('SRCTREECOVEREDTASKS = "do_validate_branches do_kernel_checkout do_fetch do_unpack do_patch"\n')
+            f.write('SRCTREECOVEREDTASKS = "do_validate_branches do_kernel_checkout '
+                    'do_fetch do_unpack do_patch do_kernel_configme do_kernel_configcheck"\n')
         if initial_rev:
             f.write('\n# initial_rev: %s\n' % initial_rev)
             for commit in commits:
