3e3abf0753a3bf1737dc4f476df4e70d31070391
Aníbal Limón <anibal.limon@linux.intel.com>
2016-02-02 16:37:36 -0600

    oeqa/sdkext/devtool.py: Add location test to ensure that devtool is the eSDK one.
    Signed-off-by: Aníbal Limón <anibal.limon@linux.intel.com>
    Signed-off-by: Ross Burton <ross.burton@intel.com>

--- meta/lib/oeqa/sdkext/devtool.py
+++ meta/lib/oeqa/sdkext/devtool.py
@@ -11,10 +11,17 @@ class DevtoolTest(oeSDKExtTest):
         self.myapp_dst = os.path.join(self.tc.sdktestdir, "myapp")
         shutil.copytree(self.myapp_src, self.myapp_dst)
 
+    def test_devtool_location(self):
+        output = self._run('which devtool')
+        self.assertEqual(output.startswith(self.tc.sdktestdir), True, \
+            msg="Seems that devtool isn't the eSDK one: %s" % output)
+
+    @skipUnlessPassed('test_devtool_location')
     def test_devtool_add_reset(self):
         self._run('devtool add myapp %s' % self.myapp_dst)
         self._run('devtool reset myapp')
 
+    @skipUnlessPassed('test_devtool_location')
     def test_devtool_build(self):
         self._run('devtool add myapp %s' % self.myapp_dst)
         self._run('devtool build myapp')
