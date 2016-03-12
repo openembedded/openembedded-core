f7b38ec43a1090ff2c56f26fe19637fba2b1a996
Richard Purdie <richard.purdie@linuxfoundation.org>
2016-03-12 22:34:45 +0000

    oeqa/recipetool: Improve debugging output by adding dirlist
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oeqa/selftest/recipetool.py
+++ meta/lib/oeqa/selftest/recipetool.py
@@ -411,7 +411,7 @@ class RecipetoolTests(RecipetoolBase):
         result = runCmd('recipetool create %s -o %s' % (srcuri, temprecipe))
         dirlist = os.listdir(temprecipe)
         if len(dirlist) < 1 or not os.path.isfile(os.path.join(temprecipe, dirlist[0])):
-            self.fail('recipetool did not create recipe file; output:\n%s' % result.output)
+            self.fail('recipetool did not create recipe file; output:\n%s\ndirlist:\n%s' % (result.output, str(dirlist))
         self.assertEqual(dirlist[0], 'socat_%s.bb' % pv, 'Recipe file incorrectly named')
         checkvars = {}
         checkvars['LICENSE'] = set(['Unknown', 'GPLv2'])
