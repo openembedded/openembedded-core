openembedded/openembedded-core

commit 8981ed20234c42dc1f5cdef802ebe7214bd55238

packagegroup: Set ALLOW_EMPTY on a per package basis
ALLOW_EMPTY should be set on a per package basis, this updates the code
to do this, avoiding warnings.

Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

meta/classes/packagegroup.bbclass
@@ -1,9 +1,5 @@
 # Class for packagegroup (package group) recipes
 
-# packagegroup packages are only used to pull in other packages
-# via their dependencies. They are empty.
-ALLOW_EMPTY = "1"
-
 # By default, only the packagegroup package itself is in PACKAGES.
 # -dbg and -dev flavours are handled by the anonfunc below.
 # This means that packagegroup recipes used to build multiple packagegroup
@@ -18,16 +14,16 @@ inherit allarch
 # This automatically adds -dbg and -dev flavours of all PACKAGES
 # to the list. Their dependencies (RRECOMMENDS) are handled as usual
 # by package_depchains in a following step.
+# Also mark all packages as ALLOW_EMPTY
 python () {
-    if d.getVar('PACKAGEGROUP_DISABLE_COMPLEMENTARY', True) == '1':
-        return
-
     packages = d.getVar('PACKAGES', True).split()
     genpackages = []
     for pkg in packages:
+        d.setVar("ALLOW_EMPTY_%s" % pkg, "1")
         for postfix in ['-dbg', '-dev', '-ptest']:
             genpackages.append(pkg+postfix)
-    d.setVar('PACKAGES', ' '.join(packages+genpackages))
+    if d.getVar('PACKAGEGROUP_DISABLE_COMPLEMENTARY', True) != '1':
+        d.setVar('PACKAGES', ' '.join(packages+genpackages))
 }
 
 # We don't want to look at shared library dependencies for the

commit 340e9d619c881f320070474d5c4674dcaeabad35

classes/packagegroup: fix dbg/dev/ptest complementary packages
OE-Core commit 8981ed20234c42dc1f5cdef802ebe7214bd55238 (from 2013)
caused a regression where -dbg/-dev/-ptest packages weren't generated
for packagegroups because we weren't setting ALLOW_EMPTY for them.
Break out the code for adding complementary groups to its own
conditional so we can set ALLOW_EMPTY at the same time.

Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>
Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

meta/classes/packagegroup.bbclass
@@ -22,12 +22,15 @@ inherit ${@oe.utils.ifelse(d.getVar('PACKAGE_ARCH_EXPANDED', True) == 'all', 'al
 # Also mark all packages as ALLOW_EMPTY
 python () {
     packages = d.getVar('PACKAGES', True).split()
-    genpackages = []
     for pkg in packages:
         d.setVar("ALLOW_EMPTY_%s" % pkg, "1")
-        for postfix in ['-dbg', '-dev', '-ptest']:
-            genpackages.append(pkg+postfix)
     if d.getVar('PACKAGEGROUP_DISABLE_COMPLEMENTARY', True) != '1':
+        # Add complementary packagegroups
+        genpackages = []
+        for pkg in packages:
+            for postfix in ['-dbg', '-dev', '-ptest']:
+                genpackages.append(pkg+postfix)
+                d.setVar("ALLOW_EMPTY_%s" % pkg+postfix, "1")
         d.setVar('PACKAGES', ' '.join(packages+genpackages))
 }
 