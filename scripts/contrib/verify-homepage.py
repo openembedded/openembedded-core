ec3f1759e8b491a44a1fc1ecb6f89919dd30da97
Ed Bartosh <ed.bartosh@linux.intel.com>
2016-06-02 13:12:50 +0300

    scripts: python3: fix urllib imports
    Some functions and classes have been moved from urllib[2]
    to urllib.request and urllib.error in python 3.
    
    Used new imports to make the code working in python 3.
    
    Signed-off-by: Ed Bartosh <ed.bartosh@linux.intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- scripts/contrib/verify-homepage.py
+++ scripts/contrib/verify-homepage.py
@@ -7,7 +7,7 @@
 import sys
 import os
 import subprocess
-import urllib2
+import urllib.request
 
 
 # Allow importing scripts/lib modules
@@ -47,7 +47,7 @@ def verifyHomepage(bbhandler):
             homepage = data.getVar("HOMEPAGE", True)
             if homepage:
                 try:
-                    urllib2.urlopen(homepage, timeout=5)
+                    urllib.request.urlopen(homepage, timeout=5)
                 except Exception:
                     count = count + wgetHomepage(os.path.basename(realfn), homepage)
             checked.append(realfn)
