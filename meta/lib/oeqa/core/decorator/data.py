a81045f4e2b740173237f5ae4e80e2bc0b287faa
Mariano Lopez <mariano.lopez@linux.intel.com>
2016-12-12 10:12:15 +0000

    oeqa/core/decorator: Add skipIfNotDataVar and skipIfNotInDataVar
    skipIfNotDataVar will skip a test if a variable doesn't have certain value.
    
    skipIfNotInDataVar will skip a test if a value is not in a certain variable.
    
    Signed-off-by: Mariano Lopez <mariano.lopez@linux.intel.com>

--- meta/lib/oeqa/core/decorator/data.py
+++ meta/lib/oeqa/core/decorator/data.py
@@ -28,9 +28,43 @@ class skipIfDataVar(OETestDecorator):
     attrs = ('var', 'value', 'msg')
 
     def setUpDecorator(self):
-        msg = 'Checking if %r value is %r to skip test' % (self.var, self.value)
+        msg = ('Checking if %r value is %r to skip test' %
+               (self.var, self.value))
         self.logger.debug(msg)
         if self.case.td.get(self.var) == self.value:
+            self.case.skipTest(self.msg)
+
+@registerDecorator
+class skipIfNotDataVar(OETestDecorator):
+    """
+        Skip test based on value of a data store's variable.
+
+        It will get the info of var from the data store and will
+        check it against value; if are not equal it will skip the
+        test with msg as the reason.
+    """
+
+    attrs = ('var', 'value', 'msg')
+
+    def setUpDecorator(self):
+        msg = ('Checking if %r value is not %r to skip test' %
+               (self.var, self.value))
+        self.logger.debug(msg)
+        if not self.case.td.get(self.var) == self.value:
+            self.case.skipTest(self.msg)
+
+@registerDecorator
+class skipIfNotInDataVar(OETestDecorator):
+    """
+        Skip test if value is not in data store's variable.
+    """
+
+    attrs = ('var', 'value', 'msg')
+    def setUpDecorator(self):
+        msg = ('Checking if %r value is in %r to run '
+              'the test' % (self.var, self.value))
+        self.logger.debug(msg)
+        if not self.value in self.case.td.get(self.var):
             self.case.skipTest(self.msg)
 
 @registerDecorator
