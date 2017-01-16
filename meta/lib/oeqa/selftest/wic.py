885a5beae86a55cc228fb2aa998a8b1135371eb4
Ed Bartosh <ed.bartosh@linux.intel.com>
2017-01-16 15:14:14 +0200

    selftest: stop using hddimg in the wic test suite
    Removed hddimg from IMAGE_FEATURES as wic code doesn't
    use hddimg anymore.
    
    [YOCTO #10835]
    
    Signed-off-by: Ed Bartosh <ed.bartosh@linux.intel.com>

--- meta/lib/oeqa/selftest/wic.py
+++ meta/lib/oeqa/selftest/wic.py
@@ -42,8 +42,7 @@ class Wic(oeSelfTest):
 
     def setUpLocal(self):
         """This code is executed before each test method."""
-        self.write_config('IMAGE_FSTYPES += " hddimg"\n'
-                          'MACHINE_FEATURES_append = " efi"\n')
+        self.write_config('MACHINE_FEATURES_append = " efi"\n')
 
         # Do this here instead of in setUpClass as the base setUp does some
         # clean up which can result in the native tools built earlier in
