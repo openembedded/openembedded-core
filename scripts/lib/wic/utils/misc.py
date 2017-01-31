9d14cb8bb8cf9559b7ff0efa03427624c18e2506
Ed Bartosh <ed.bartosh@linux.intel.com>
2017-01-31 12:45:01 +0200

    wci: misc: removed build_name API
    This API is not used in wic code.
    
    [YOCTO #10619]
    
    Signed-off-by: Ed Bartosh <ed.bartosh@linux.intel.com>

--- scripts/lib/wic/utils/misc.py
+++ scripts/lib/wic/utils/misc.py
@@ -19,45 +19,6 @@ import os
 import time
 import wic.engine
 
-def build_name(kscfg, release=None, prefix=None, suffix=None):
-    """Construct and return an image name string.
-
-    This is a utility function to help create sensible name and fslabel
-    strings. The name is constructed using the sans-prefix-and-extension
-    kickstart filename and the supplied prefix and suffix.
-
-    kscfg -- a path to a kickstart file
-    release --  a replacement to suffix for image release
-    prefix -- a prefix to prepend to the name; defaults to None, which causes
-              no prefix to be used
-    suffix -- a suffix to append to the name; defaults to None, which causes
-              a YYYYMMDDHHMM suffix to be used
-
-    Note, if maxlen is less then the len(suffix), you get to keep both pieces.
-
-    """
-    name = os.path.basename(kscfg)
-    idx = name.rfind('.')
-    if idx >= 0:
-        name = name[:idx]
-
-    if release is not None:
-        suffix = ""
-    if prefix is None:
-        prefix = ""
-    if suffix is None:
-        suffix = time.strftime("%Y%m%d%H%M")
-
-    if name.startswith(prefix):
-        name = name[len(prefix):]
-
-    prefix = "%s-" % prefix if prefix else ""
-    suffix = "-%s" % suffix if suffix else ""
-
-    ret = prefix + name + suffix
-
-    return ret
-
 def find_canned(scripts_path, file_name):
     """
     Find a file either by its path or by name in the canned files dir.
