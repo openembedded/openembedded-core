233d36b0382a8b2e430c3377e50885d1a0c3ba21
brian avery <brian.avery@intel.com>
2017-03-18 08:08:47 -0700

    meta/lib/oeqa: change sdk test from cvs to cpio
    We currently fetch,configure,build, and install cvs as our test for the
    sdk.  cvs unfortunately, requires a default editor in order to run.  The
    change in 94790a8254d6 that checks to see if you have something like vi
    installed is fragile since you may have a different default editor. This
    patch switches from using cvs as a test to using cpio. cpio also uses
    autotools so the functionality tested is equivalent.
    
    Signed-off-by: brian avery <brian.avery@intel.com>
    Signed-off-by: Ross Burton <ross.burton@intel.com>

--- meta/lib/oeqa/runtime/cases/buildcpio.py
+++ meta/lib/oeqa/runtime/cases/buildcpio.py
@@ -5,12 +5,12 @@ from oeqa.core.decorator.data import skipIfNotFeature
 
 from oeqa.runtime.utils.targetbuildproject import TargetBuildProject
 
-class BuildCvsTest(OERuntimeTestCase):
+class BuildCpioTest(OERuntimeTestCase):
 
     @classmethod
     def setUpClass(cls):
-        uri = 'http://ftp.gnu.org/non-gnu/cvs/source/feature/1.12.13'
-        uri = '%s/cvs-1.12.13.tar.bz2' % uri
+        uri = 'https://ftp.gnu.org/gnu/cpio'
+        uri = '%s/cpio-2.12.tar.bz2' % uri
         cls.project = TargetBuildProject(cls.tc.target,
                                          uri,
                                          dl_dir = cls.tc.td['DL_DIR'])
@@ -24,7 +24,7 @@ class BuildCvsTest(OERuntimeTestCase):
     @skipIfNotFeature('tools-sdk',
                       'Test requires tools-sdk to be in IMAGE_FEATURES')
     @OETestDepends(['ssh.SSHTest.test_ssh'])
-    def test_cvs(self):
+    def test_cpio(self):
         self.project.run_configure()
         self.project.run_make()
         self.project.run_install()
--- meta/lib/oeqa/sdk/cases/buildcpio.py
+++ meta/lib/oeqa/sdk/cases/buildcpio.py
@@ -2,15 +2,15 @@ import unittest
 from oeqa.sdk.case import OESDKTestCase
 from oeqa.sdk.utils.sdkbuildproject import SDKBuildProject
 
-class BuildCvsTest(OESDKTestCase):
+class BuildCpioTest(OESDKTestCase):
     td_vars = ['DATETIME']
 
     @classmethod
     def setUpClass(self):
         dl_dir = self.td.get('DL_DIR', None)
 
-        self.project = SDKBuildProject(self.tc.sdk_dir + "/cvs/", self.tc.sdk_env,
-                        "http://ftp.gnu.org/non-gnu/cvs/source/feature/1.12.13/cvs-1.12.13.tar.bz2",
+        self.project = SDKBuildProject(self.tc.sdk_dir + "/cpio/", self.tc.sdk_env,
+                        "https://ftp.gnu.org/gnu/cpio/cpio-2.12.tar.bz2",
                         self.tc.sdk_dir, self.td['DATETIME'], dl_dir=dl_dir)
         self.project.download_archive()
 
@@ -18,7 +18,7 @@ class BuildCvsTest(OESDKTestCase):
         if not self.tc.hasHostPackage("packagegroup-cross-canadian-%s" % machine):
             raise unittest.SkipTest("SDK doesn't contain a cross-canadian toolchain")
 
-    def test_cvs(self):
+    def test_cpio(self):
         self.assertEqual(self.project.run_configure(), 0,
                         msg="Running configure failed")
 
