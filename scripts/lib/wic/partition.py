be93e378506a85772503005294503cfc348a552c
Daniel Schultz <d.schultz@phytec.de>
2017-03-30 09:07:13 +0200

    wic: partition: Update fsck parameters
    These parameters are copied from the ext image class.
    
    -D will let fsck perform further directory optimizations
    -v might be helpful for debugging purpose
    
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>
    Signed-off-by: Ross Burton <ross.burton@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- scripts/lib/wic/partition.py
+++ scripts/lib/wic/partition.py
@@ -267,7 +267,7 @@ class Partition():
             (self.fstype, extra_imagecmd, rootfs, label_str, rootfs_dir)
         exec_native_cmd(mkfs_cmd, native_sysroot, pseudo=pseudo)
 
-        mkfs_cmd = "fsck.%s -fy %s" % (self.fstype, rootfs)
+        mkfs_cmd = "fsck.%s -pvfD %s" % (self.fstype, rootfs)
         exec_native_cmd(mkfs_cmd, native_sysroot, pseudo=pseudo)
 
     def prepare_rootfs_btrfs(self, rootfs, oe_builddir, rootfs_dir,
