230452faf151e277bfb2b49526923f8097755b35
Ed Bartosh <ed.bartosh@linux.intel.com>
2017-03-30 17:18:03 +0300

    wic: set correct system id for msdos partitions
    Explicitly set system id 0x6(FAT16) for msdos partitions.
    
    Removed old code that attempts to achieve the same result
    using 'parted ... lba off'.
    
    Signed-off-by: Ed Bartosh <ed.bartosh@linux.intel.com>

--- scripts/lib/wic/plugins/imager/direct.py
+++ scripts/lib/wic/plugins/imager/direct.py
@@ -487,6 +487,8 @@ class PartitionedImage():
                 parted_fs_type = "fat32"
             elif part.fstype == "msdos":
                 parted_fs_type = "fat16"
+                if not part.system_id:
+                    part.system_id = '0x6' # FAT16
             else:
                 # Type for ext2/ext3/ext4/btrfs
                 parted_fs_type = "ext2"
@@ -535,17 +537,6 @@ class PartitionedImage():
                 exec_native_cmd("sfdisk --part-type %s %s %s" % \
                                 (self.path, part.num, part.system_id),
                                 self.native_sysroot)
-
-            # Parted defaults to enabling the lba flag for fat16 partitions,
-            # which causes compatibility issues with some firmware (and really
-            # isn't necessary).
-            if parted_fs_type == "fat16":
-                if self.ptable_format == 'msdos':
-                    logger.debug("Disable 'lba' flag for partition '%s' on disk '%s'",
-                                 part.num, self.path)
-                    exec_native_cmd("parted -s %s set %d lba off" % \
-                                    (self.path, part.num),
-                                    self.native_sysroot)
 
     def cleanup(self):
         # remove partition images
