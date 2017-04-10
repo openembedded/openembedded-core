809b23c829f352c0eae455ea89f53e2a9ee87f06
Richard Purdie <richard.purdie@linuxfoundation.org>
2017-04-10 17:00:37 +0100

    scripts: Drop cleanup-workdir
    This script appears broken and is actively breaking build directories.
    For example, binutils-cross-i586 gets run do_fetch to do_populate_sysroot by:
    
    $ oe-selftest -r devtool.DevtoolTests.test_devtool_virtual_kernel_modify
    
    then:
    
    $ oe-selftest -r oescripts.BuildhistoryDiffTests.test_buildhistory_diff
    
    wipes out the contents of tmp/work/x86_64-linux/binutils-cross-i586/2.28-r0/
    but does not wipe out the corresponding stamps, then:
    
    $ oe-selftest -r runtime-test.Postinst.test_postinst_rootfs_and_boot
    
    needs binutils-cross-i586:do_populate_lic and if and only if this is
    unavailable from sstate, it fails since it thinks the source is already
    unpacked when it isn't resulting in:
    
    WARNING: binutils-cross-i586-2.28-r0 do_populate_lic: Could not copy license file /home/pokybuild/yocto-autobuilder/yocto-worker/nigh
    tly-oe-selftest/build/build/tmp/work/x86_64-linux/binutils-cross-i586/2.28-r0/git/COPYING3.LIB to /home/pokybuild/yocto-autobuilder/yocto
    -worker/nightly-oe-selftest/build/build/tmp/work/x86_64-linux/binutils-cross-i586/2.28-r0/license-destdir/binutils-cross-i586/COPYING3.LI
    B: [Errno 2] No such file or directory: '/home/pokybuild/yocto-autobuilder/yocto-worker/nightly-oe-selftest/build/build/tmp/work/x86_64-l
    inux/binutils-cross-i586/2.28-r0/git/COPYING3.LIB'
    ERROR: binutils-cross-i586-2.28-r0 do_populate_lic: QA Issue: binutils-cross-i586: LIC_FILES_CHKSUM points to an invalid file: /home/
    pokybuild/yocto-autobuilder/yocto-worker/nightly-oe-selftest/build/build/tmp/work/x86_64-linux/binutils-cross-i586/2.28-r0/git/COPYING3 [license-checksum]
    
    and similar errors.
    
    Its safer for users to wipe tmp than try and maintain scripts which try
    to remove pieces of tmp and get it wrong so remove the script.
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oeqa/selftest/oescripts.py
+++ meta/lib/oeqa/selftest/oescripts.py
@@ -10,33 +10,6 @@ from oeqa.selftest.buildhistory import BuildhistoryBase
 from oeqa.utils.commands import Command, runCmd, bitbake, get_bb_var, get_test_layer
 from oeqa.utils.decorators import testcase
 
-class TestScripts(oeSelfTest):
-
-    @testcase(300)
-    def test_cleanup_workdir(self):
-        path = os.path.dirname(get_bb_var('WORKDIR', 'selftest-ed'))
-        old_version_recipe = os.path.join(get_bb_var('COREBASE'), 'meta-selftest/recipes-test/selftest-ed/selftest-ed_0.5.bb')
-        old_version = '0.5'
-        bitbake("-c clean selftest-ed")
-        bitbake("-c clean -b %s" % old_version_recipe)
-
-        if os.path.exists(path):
-            initial_contents = os.listdir(path)
-        else:
-            initial_contents = []
-
-        bitbake('selftest-ed')
-        intermediary_contents = os.listdir(path)
-        bitbake("-b %s" % old_version_recipe)
-        runCmd('cleanup-workdir')
-        remaining_contents = os.listdir(path)
-
-        expected_contents = [x for x in intermediary_contents if x not in initial_contents]
-        remaining_not_expected = [x for x in remaining_contents if x not in expected_contents]
-        self.assertFalse(remaining_not_expected, msg="Not all necessary content has been deleted from %s: %s" % (path, ', '.join(map(str, remaining_not_expected))))
-        expected_not_remaining = [x for x in expected_contents if x not in remaining_contents]
-        self.assertFalse(expected_not_remaining, msg="The script removed extra contents from %s: %s" % (path, ', '.join(map(str, expected_not_remaining))))
-
 class BuildhistoryDiffTests(BuildhistoryBase):
 
     @testcase(295)
