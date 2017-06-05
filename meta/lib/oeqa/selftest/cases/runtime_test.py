2f6a506018973f0eaf1306555df3e74a3ab25680
Humberto Ibarra <humberto.ibarra.lopez@intel.com>
2017-06-05 12:19:34 -0500

    runtime-test.py: Split different tests inside selftest.py
    There were two completely different tests inside selftest.py,
    and the only reason for them to be together was that both needed
    the same mechanism to execute (run testimage from within selftest)
    
    This fixes the design issue and error-prone situation by separating
    these tests. In add a new module for dnf-runtime and also has an
    extra time added to running the tests, but it is minimal since
    the builds reuse data from previuous build.
    
    [YOCTO #11436]
    
    Signed-off-by: Humberto Ibarra <humberto.ibarra.lopez@intel.com>
    Signed-off-by: Ross Burton <ross.burton@intel.com>

--- meta/lib/oeqa/selftest/cases/runtime_test.py
+++ meta/lib/oeqa/selftest/cases/runtime_test.py
@@ -109,16 +109,32 @@ class TestImage(OESelftestTestCase):
         Summary: Check install packages functionality for testimage/testexport.
         Expected: 1. Import tests from a directory other than meta.
                   2. Check install/uninstall of socat.
-                  3. Check that remote package feeds can be accessed
         Product: oe-core
         Author: Mariano Lopez <mariano.lopez@intel.com>
-        Author: Alexander Kanavin <alexander.kanavin@intel.com>
         """
         if get_bb_var('DISTRO') == 'poky-tiny':
             self.skipTest('core-image-full-cmdline not buildable for poky-tiny')
 
         features = 'INHERIT += "testimage"\n'
         features += 'TEST_SUITES = "ping ssh selftest"\n'
+        self.write_config(features)
+
+        # Build core-image-sato and testimage
+        bitbake('core-image-full-cmdline socat')
+        bitbake('-c testimage core-image-full-cmdline')
+
+    def test_testimage_dnf(self):
+        """
+        Summary: Check package feeds functionality for dnf
+        Expected: 1. Check that remote package feeds can be accessed
+        Product: oe-core
+        Author: Alexander Kanavin <alexander.kanavin@intel.com>
+        """
+        if get_bb_var('DISTRO') == 'poky-tiny':
+            self.skipTest('core-image-full-cmdline not buildable for poky-tiny')
+
+        features = 'INHERIT += "testimage"\n'
+        features += 'TEST_SUITES = "ping ssh dnf-runtime"\n'
         # We don't yet know what the server ip and port will be - they will be patched
         # in at the start of the on-image test
         features += 'PACKAGE_FEED_URIS = "http://bogus_ip:bogus_port"\n'
