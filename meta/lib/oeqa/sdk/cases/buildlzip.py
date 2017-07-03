0699de9efe40029a6c5e799bb8c9616337fd163e
Jose Perez Carranza <jose.perez.carranza@linux.intel.com>
2017-07-03 13:04:15 -0700

    oeqa/sdk: Replace buildiptables for buildlzip tests
    Buildiptables test cases are conflicting with images built with “musl”
    as standard C library, in order to avoid those issues lzip package was
    selected to be used on the tests as this does not have any "musl"
    dependency.
    
    [YOCTO #11713]
    
    (From OE-Core rev: b798284f62b3cb171373716b1ee84403439314aa)
    
    Signed-off-by: Jose Perez Carranza <jose.perez.carranza@linux.intel.com>
    Signed-off-by: Ross Burton <ross.burton@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>
    Signed-off-by: Armin Kuster <akuster808@gmail.com>

--- meta/lib/oeqa/sdk/cases/buildlzip.py
+++ meta/lib/oeqa/sdk/cases/buildlzip.py
@@ -3,15 +3,15 @@ from oeqa.sdk.case import OESDKTestCase
 from oeqa.sdk.utils.sdkbuildproject import SDKBuildProject
 
 
-class BuildIptablesTest(OESDKTestCase):
+class BuildLzipTest(OESDKTestCase):
     td_vars = ['DATETIME']
 
     @classmethod
     def setUpClass(self):
         dl_dir = self.td.get('DL_DIR', None)
 
-        self.project = SDKBuildProject(self.tc.sdk_dir + "/iptables/", self.tc.sdk_env, 
-                        "http://downloads.yoctoproject.org/mirror/sources/iptables-1.4.13.tar.bz2",
+        self.project = SDKBuildProject(self.tc.sdk_dir + "/lzip/", self.tc.sdk_env,
+                        "http://downloads.yoctoproject.org/mirror/sources/lzip-1.19.tar.gz",
                         self.tc.sdk_dir, self.td['DATETIME'], dl_dir=dl_dir)
         self.project.download_archive()
 
@@ -20,7 +20,7 @@ class BuildIptablesTest(OESDKTestCase):
         if not self.tc.hasHostPackage("packagegroup-cross-canadian-%s" % machine):
             raise unittest.SkipTest("SDK doesn't contain a cross-canadian toolchain")
 
-    def test_iptables(self):
+    def test_lzip(self):
         self.assertEqual(self.project.run_configure(), 0,
                         msg="Running configure failed")
 
