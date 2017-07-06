d40bcafb574788ed26855c5d1a072523893c1b4b
Jose Perez Carranza <jose.perez.carranza@linux.intel.com>
2017-07-06 14:03:07 -0700

    oeqa/runtime: Replace buildiptables for buildlzip on runtime tests
    Buildiptables test cases are conflicting with images built with “musl”
    as standard C library, in order to avoid those issues lzip package was
    selected to be used on the tests as this does not have any "musl"
    dependency.
    
    This patch is applicable for testimage tests
    
    [YOCTO # 11713]
    
    (From OE-Core rev: 41683e0ab316049e28b1f4ceaf39f0fe17722d92)
    
    Signed-off-by: Jose Perez Carranza <jose.perez.carranza@linux.intel.com>
    Signed-off-by: Ross Burton <ross.burton@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>
    Signed-off-by: Armin Kuster <akuster808@gmail.com>

--- meta/lib/oeqa/runtime/cases/buildlzip.py
+++ meta/lib/oeqa/runtime/cases/buildlzip.py
@@ -5,12 +5,12 @@ from oeqa.core.decorator.data import skipIfNotFeature
 
 from oeqa.runtime.utils.targetbuildproject import TargetBuildProject
 
-class BuildIptablesTest(OERuntimeTestCase):
+class BuildLzipTest(OERuntimeTestCase):
 
     @classmethod
     def setUpClass(cls):
         uri = 'http://downloads.yoctoproject.org/mirror/sources'
-        uri = '%s/iptables-1.4.13.tar.bz2' % uri
+        uri = '%s/lzip-1.19.tar.gz' % uri
         cls.project = TargetBuildProject(cls.tc.target,
                                          uri,
                                          dl_dir = cls.tc.td['DL_DIR'])
@@ -24,7 +24,7 @@ class BuildIptablesTest(OERuntimeTestCase):
     @skipIfNotFeature('tools-sdk',
                       'Test requires tools-sdk to be in IMAGE_FEATURES')
     @OETestDepends(['ssh.SSHTest.test_ssh'])
-    def test_iptables(self):
+    def test_lzip(self):
         self.project.run_configure()
         self.project.run_make()
         self.project.run_install()
