7fca44e03130c0860cc5df2093902773f426c774
Ed Bartosh <ed.bartosh@linux.intel.com>
2017-09-06 15:08:25 +0300

    wic: run bmaptool with native Python3
    Modified wic code to run bmaptool using native Python3
    from wic-tools native sysroot.
    
    [YOCTO #11891]
    
    Signed-off-by: Ed Bartosh <ed.bartosh@linux.intel.com>
    Signed-off-by: Ross Burton <ross.burton@intel.com>

--- scripts/lib/wic/plugins/imager/direct.py
+++ scripts/lib/wic/plugins/imager/direct.py
@@ -213,8 +213,10 @@ class DirectPlugin(ImagerPlugin):
         # Generate .bmap
         if self.bmap:
             logger.debug("Generating bmap file for %s", disk_name)
-            exec_native_cmd("bmaptool create %s -o %s.bmap" % (full_path, full_path),
-                            self.native_sysroot)
+            python = os.path.join(self.native_sysroot, 'usr/bin/python3-native/python3')
+            bmaptool = os.path.join(self.native_sysroot, 'usr/bin/bmaptool')
+            exec_native_cmd("%s %s create %s -o %s.bmap" % \
+                            (python, bmaptool, full_path, full_path), self.native_sysroot)
         # Compress the image
         if self.compressor:
             logger.debug("Compressing disk %s with %s", disk_name, self.compressor)
