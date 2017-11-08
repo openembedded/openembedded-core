ba8babc45141891d0624f9a181a580fa416e87ec
Richard Purdie <richard.purdie@linuxfoundation.org>
2017-11-08 18:17:17 +0000

    oeqa: Clean up logger handling
    The logger handling in oeqa was confused at best. This patch:
    
    a) Passes in a logger through various qemu runner pieces
    b) Uses that logger consistently in the code
    c) Creates a logger for QemuRunner outside the bitbake namespace
       meaning we don't conflict with the tinfoil logging changes
    
    The result of this is more consistency. For runtime tests in testimage,
    the logs always contain the debug info, nothing is shwon on the console.
    For the oe-selftests, logs are intercepted and only shown if the test
    fails.
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>
    Signed-off-by: Armin Kuster <akuster@mvista.com>

--- meta/lib/oeqa/selftest/cases/runqemu.py
+++ meta/lib/oeqa/selftest/cases/runqemu.py
@@ -3,7 +3,6 @@
 #
 
 import re
-import logging
 
 from oeqa.selftest.case import OESelftestTestCase
 from oeqa.utils.commands import bitbake, runqemu, get_bb_var
@@ -23,10 +22,6 @@ class RunqemuTests(OESelftestTestCase):
         self.machine =  'qemux86-64'
         self.fstypes = "ext4 iso hddimg wic.vmdk wic.qcow2 wic.vdi"
         self.cmd_common = "runqemu nographic"
-
-        # Avoid emit the same record multiple times.
-        mainlogger = logging.getLogger("BitBake.Main")
-        mainlogger.propagate = False
 
         self.write_config(
 """
