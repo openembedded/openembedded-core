9128fd1396729a71b4832a597cf070c2be922d63
Alexander Kanavin <alexander.kanavin@linux.intel.com>
2018-05-24 12:36:44 +0300

    package_manager.py: get rid of ROOTFS_RPM_DEBUG in RpmPM()
    This was undocumented, and it's better to just always enable
    full debug output, as this allows immediate generation of logs
    with full diagnostics when things go not as expected.
    
    Also, change the output of dnf from note to debug level; this
    does not affect what is written to log file, but does reduce the
    verbosity of bitbake -v.
    
    Signed-off-by: Alexander Kanavin <alexander.kanavin@linux.intel.com>
    Signed-off-by: Ross Burton <ross.burton@intel.com>

--- meta/lib/oe/package_manager.py
+++ meta/lib/oe/package_manager.py
@@ -888,7 +888,7 @@ class RpmPM(PackageManager):
         os.environ['RPM_ETCCONFIGDIR'] = self.target_rootfs
 
         dnf_cmd = bb.utils.which(os.getenv('PATH'), "dnf")
-        standard_dnf_args = (["-v", "--rpmverbosity=debug"] if self.d.getVar('ROOTFS_RPM_DEBUG') else []) + ["-y",
+        standard_dnf_args = ["-v", "--rpmverbosity=debug", "-y",
                              "-c", oe.path.join(self.target_rootfs, "etc/dnf/dnf.conf"),
                              "--setopt=reposdir=%s" %(oe.path.join(self.target_rootfs, "etc/yum.repos.d")),
                              "--repofrompath=oe-repo,%s" % (self.rpm_repo_dir),
@@ -900,7 +900,7 @@ class RpmPM(PackageManager):
         try:
             output = subprocess.check_output(cmd,stderr=subprocess.STDOUT).decode("utf-8")
             if print_output:
-                bb.note(output)
+                bb.debug(1, output)
             return output
         except subprocess.CalledProcessError as e:
             if print_output:
