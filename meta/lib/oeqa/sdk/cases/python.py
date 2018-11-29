d286c2ad3eec24978557e16a8fa599476791109f
Ross Burton <ross.burton@intel.com>
2018-11-29 17:39:33 +0000

    oeqa/sdk/python: add Python 2 and fix detection
    Add a Python 2 form to exercise that if present, and fix the setUp() so it
    actually looks for a package that exists (nativesdk-python3 is a virtual
    package, the interpretter is in nativesdk-python3-core).
    
    Signed-off-by: Ross Burton <ross.burton@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oeqa/sdk/cases/python.py
+++ meta/lib/oeqa/sdk/cases/python.py
@@ -1,11 +1,25 @@
 import subprocess, unittest
 from oeqa.sdk.case import OESDKTestCase
 
-class PythonTest(OESDKTestCase):
+class Python2Test(OESDKTestCase):
     def setUp(self):
-        if not (self.tc.hasHostPackage("nativesdk-python3") or
-                self.tc.hasHostPackage("python3-native")):
+        if not (self.tc.hasHostPackage("nativesdk-python-core") or
+                self.tc.hasHostPackage("python-core-native")):
             raise unittest.SkipTest("No python package in the SDK")
+
+    def test_python3(self):
+        try:
+            cmd = "python -c \"import codecs; print(codecs.encode('Uryyb, jbeyq', 'rot13'))\""
+            output = self._run(cmd)
+            self.assertEqual(output, "Hello, world\n")
+        except subprocess.CalledProcessError as e:
+            self.fail("Unexpected exit %d (output %s)" % (e.returncode, e.output))
+
+class Python3Test(OESDKTestCase):
+    def setUp(self):
+        if not (self.tc.hasHostPackage("nativesdk-python3-core") or
+                self.tc.hasHostPackage("python3-core-native")):
+            raise unittest.SkipTest("No python3 package in the SDK")
 
     def test_python3(self):
         try:
