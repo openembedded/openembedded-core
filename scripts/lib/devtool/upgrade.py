29e064ac6bd8d1ef74c280bf85bf8428c37ba014
Ross Burton <ross.burton@intel.com>
2019-02-01 17:08:39 +0000

    devtool: add --all option to check-upgrade-status
    The default behaviour is to show just recipes needing upgrades, but for
    automated reports showing all recipes can be useful.
    
    Signed-off-by: Ross Burton <ross.burton@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- scripts/lib/devtool/upgrade.py
+++ scripts/lib/devtool/upgrade.py
@@ -606,7 +606,7 @@ def check_upgrade_status(args, config, basepath, workspace):
     results = oe.recipeutils.get_recipe_upgrade_status(args.recipe)
     for result in results:
         # pn, update_status, current, latest, maintainer, latest_commit, no_update_reason
-        if result[1] != 'MATCH':
+        if args.all or result[1] != 'MATCH':
             logger.info("{:25} {:15} {:15} {} {} {}".format(   result[0],
                                                                result[2],
                                                                result[1] if result[1] != 'UPDATE' else (result[3] if not result[3].endswith("new-commits-available") else "new commits"),
@@ -646,4 +646,5 @@ def register_commands(subparsers, context):
                                                         description="Prints a table of recipes together with versions currently provided by recipes, and latest upstream versions, when there is a later version available",
                                                         group='info')
     parser_check_upgrade_status.add_argument('recipe', help='Name of the recipe to report (omit to report upgrade info for all recipes)', nargs='*')
+    parser_check_upgrade_status.add_argument('--all', '-a', help='Show all recipes, not just recipes needing upgrade', action="store_true")
     parser_check_upgrade_status.set_defaults(func=check_upgrade_status)
