c27cdf83bc2b8ff802a5c4e0b49f18174af8e34a
Richard Purdie <richard.purdie@linuxfoundation.org>
2019-03-10 07:54:10 +0000

    meta-selftest/virgl: Exclude centos7 from the kmscube test
    This test does not work on centos7 so diable it (as was done in the
    original series before we thought it was working).
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta-selftest/lib/oeqa/runtime/cases/virgl.py
+++ meta-selftest/lib/oeqa/runtime/cases/virgl.py
@@ -1,6 +1,7 @@
 from oeqa.runtime.case import OERuntimeTestCase
 from oeqa.core.decorator.depends import OETestDepends
 import subprocess
+import oe.lsb
 
 class VirglTest(OERuntimeTestCase):
 
@@ -12,6 +13,11 @@ class VirglTest(OERuntimeTestCase):
 
     @OETestDepends(['virgl.VirglTest.test_kernel_driver'])
     def test_kmscube(self):
+
+        distro = oe.lsb.distro_identifier()
+        if distro and distro == 'centos-7':
+            self.skipTest('kmscube is not working when centos 7 is the host OS')
+
         status, output = self.target.run('kmscube', timeout=30)
         self.assertEqual(status, 0, "kmscube exited with non-zero status %d and output:\n%s" %(status, output))
         self.assertIn('renderer: "virgl"', output, "kmscube does not seem to use virgl:\n%s" %(output))
