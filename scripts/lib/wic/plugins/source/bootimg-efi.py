becb0a3f855eff7700fa284a0a7981d6a260a1c5
Chee Yang Lee <chee.yang.lee@intel.com>
2019-05-13 12:07:09 +0800

    wic/bootimg-efi: replace hardcoded volume name with label
    volume name should refer to --label in .wks.
    Replace the hardcoded volume name  with label.
    set "ESP" as default name when no lable specified.
    
    Signed-off-by: Chee Yang Lee <chee.yang.lee@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>
    Signed-off-by: Armin Kuster <akuster808@gmail.com>

--- scripts/lib/wic/plugins/source/bootimg-efi.py
+++ scripts/lib/wic/plugins/source/bootimg-efi.py
@@ -256,8 +256,10 @@ class BootimgEFIPlugin(SourcePlugin):
         # dosfs image, created by mkdosfs
         bootimg = "%s/boot.img" % cr_workdir
 
-        dosfs_cmd = "mkdosfs -n efi -i %s -C %s %d" % \
-                    (part.fsuuid, bootimg, blocks)
+        label = part.label if part.label else "ESP"
+
+        dosfs_cmd = "mkdosfs -n %s -i %s -C %s %d" % \
+                    (label, part.fsuuid, bootimg, blocks)
         exec_native_cmd(dosfs_cmd, native_sysroot)
 
         mcopy_cmd = "mcopy -i %s -s %s/* ::/" % (bootimg, hdddir)
