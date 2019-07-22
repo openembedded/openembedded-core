2df5d95b9b63e30ddaa7c72a4173b9a05b3f15f9
Richard Purdie <richard.purdie@linuxfoundation.org>
2019-07-22 17:37:13 +0100

    sstatesig: Add debug for incorrect hash server settings
    If the hash server settings are incorrect, show the user useful
    error messages instead of tracebacks.
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oe/sstatesig.py
+++ meta/lib/oe/sstatesig.py
@@ -272,7 +272,11 @@ class SignatureGeneratorOEEquivHash(bb.siggen.SignatureGeneratorUniHashMixIn, Si
     def init_rundepcheck(self, data):
         super().init_rundepcheck(data)
         self.server = data.getVar('SSTATE_HASHEQUIV_SERVER')
+        if not self.server:
+            bb.fatal("OEEquivHash requires SSTATE_HASHEQUIV_SERVER to be set")
         self.method = data.getVar('SSTATE_HASHEQUIV_METHOD')
+        if not self.method:
+            bb.fatal("OEEquivHash requires SSTATE_HASHEQUIV_METHOD to be set")
         self.unihashes = bb.persist_data.persist('SSTATESIG_UNIHASH_CACHE_v1_' + self.method.replace('.', '_'), data)
 
 
