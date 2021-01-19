d5db7e268947f0392c2126137571a44acd29ccd6
Paul Barker <pbarker@konsulko.com>
2021-01-19 16:26:08 +0000

    wic: Update pseudo db when excluding content from rootfs
    To exclude content from the rootfs, wic makes a copy (using hardlinks if
    possible) of the rootfs directory and associated pseudo db, then removes
    files & directories as needed. However if these files and directories
    are removed using the python functions os.remove and shutil.rmtree, the
    copied pseudo db will not be updated correctly. For files copied from
    the original rootfs, if hardlinks were used successfully when copying
    the rootfs this should mean that the relevant inodes can't be reused and
    so the risk of pseudo aborts should be avoided. However, this logic
    doesn't apply for directories (as they can't be hardlinked) or for files
    added via the '--include-path' argument (as they weren't present in the
    original rootfs) and so there remains some risk of inodes being reused
    and the pseudo db becoming corrupted.
    
    To fix this, use the 'rm' command under pseudo when removing files &
    directories from the copied rootfs to ensure that the copied pseudo db
    is updated.
    
    Signed-off-by: Paul Barker <pbarker@konsulko.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- scripts/lib/wic/plugins/source/rootfs.py
+++ scripts/lib/wic/plugins/source/rootfs.py
@@ -199,17 +199,20 @@ class RootfsPlugin(SourcePlugin):
                 if not os.path.lexists(full_path):
                     continue
 
+                if new_pseudo:
+                    pseudo = cls.__get_pseudo(native_sysroot, new_rootfs, new_pseudo)
+                else:
+                    pseudo = None
                 if path.endswith(os.sep):
                     # Delete content only.
                     for entry in os.listdir(full_path):
                         full_entry = os.path.join(full_path, entry)
-                        if os.path.isdir(full_entry) and not os.path.islink(full_entry):
-                            shutil.rmtree(full_entry)
-                        else:
-                            os.remove(full_entry)
+                        rm_cmd = "rm -rf %s" % (full_entry)
+                        exec_native_cmd(rm_cmd, native_sysroot, pseudo)
                 else:
                     # Delete whole directory.
-                    shutil.rmtree(full_path)
+                    rm_cmd = "rm -rf %s" % (full_path)
+                    exec_native_cmd(rm_cmd, native_sysroot, pseudo)
 
         part.prepare_rootfs(cr_workdir, oe_builddir,
                             new_rootfs or part.rootfs_dir, native_sysroot,
