58f7cd4d6186525f08f3027975530d647cbfa26b
Sakib Sajal <sakib.sajal@windriver.com>
2021-04-26 15:51:59 -0400

    oe-time-dd-test.sh: collect cooker log when timeout is exceeded
    Collect the last 30 lines from the cooker.log
    whenever the timeout is exceeded.
    
    Signed-off-by: Sakib Sajal <sakib.sajal@windriver.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- scripts/oe-time-dd-test.sh
+++ scripts/oe-time-dd-test.sh
@@ -25,4 +25,5 @@ timeout ${TIMEOUT} dd if=/dev/zero of=oe-time-dd-test.dat bs=1024 count=$1 conv=
 if [ $? -ne 0 ]; then
 	echo "Timeout used: ${TIMEOUT}"
 	top -c -b -n1 -w 512
+	tail -30 tmp*/log/cooker/*/console-latest.log
 fi
