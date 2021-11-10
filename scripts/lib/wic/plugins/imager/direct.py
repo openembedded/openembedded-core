df9dca9fe4dd1abfe5f3986389a8e8ff524da5d7
Khem Raj <raj.khem@gmail.com>
2021-11-09 22:07:33 -0800

    wic: Use os.rename instead of bb.utils.rename
    This is not invoked with bitbake context as a result bb.utils is not
    visible when this function is called during image creation and builds
    fail e.g.
    
    NameError: name 'bb' is not defined
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>
    Cc: Joshua Watt <JPEWhacker@gmail.com>
    Cc: Devendra Tewari <devendra.tewari@gmail.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- scripts/lib/wic/plugins/imager/direct.py
+++ scripts/lib/wic/plugins/imager/direct.py
@@ -619,5 +619,5 @@ class PartitionedImage():
                              part.start + part.size_sec - 1, part.size_sec)
 
                 partimage = self.path + '.p%d' % part.num
-                bb.utils.rename(source, partimage)
+                os.rename(source, partimage)
                 self.partimages.append(partimage)
