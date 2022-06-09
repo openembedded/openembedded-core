09ba96a2d7fa02c7a82758bbf4109b04ffca2c55
Richard Purdie <richard.purdie@linuxfoundation.org>
2022-06-09 11:30:39 +0100

    buildcfg: Drop unused svn revision function
    This isn't used anywhere and everyone is using git now, the functions
    are now hardcoded as such too. Drop this function.
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oe/buildcfg.py
+++ meta/lib/oe/buildcfg.py
@@ -13,17 +13,6 @@ def detect_branch(d):
 def get_scmbasepath(d):
     return os.path.join(d.getVar('COREBASE'), 'meta')
 
-def get_metadata_svn_revision(path, d):
-    # This only works with older subversion. For newer versions 
-    # this function will need to be fixed by someone interested
-    revision = "<unknown>"
-    try:
-        with open("%s/.svn/entries" % path) as f:
-            revision = f.readlines()[3].strip()
-    except (IOError, IndexError):
-        pass
-    return revision
-
 def get_metadata_git_branch(path, d):
     try:
         rev, _ = bb.process.run('git rev-parse --abbrev-ref HEAD', cwd=path)
