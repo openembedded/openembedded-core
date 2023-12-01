7ab6087536bc67c63094f08f863dcd3d5e35b8e7
Zahir Hussain <zahir.basha@kpit.com>
2023-12-01 17:46:17 +0530

    cmake: Unset CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES
    As discussion in [YOCTO #14717] cmake contains a OEToolchainConfig.cmake
    file to configure the toolchain correctly in cross-compile build for recipes
    using cmake.
    
    The variable CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES value updates incorrectly
    during do_compile the code. Due to this getting sporadic error like below,
    
    fatal error: stdlib.h: No such file or directory
    |    75 | #include_next <stdlib.h>
    |       |               ^~~~~~~~~~
    | compilation terminated.
    | ninja: build stopped: subcommand failed.
    | WARNING: exit code 1 from a shell command.
    
    As cmake already correctly initializes the variable from environment,
    So we have to unset it in the toolchain file to avoid overwriting the
    variable definition again.
    
    Signed-off-by: aszh07 <mail2szahir@gmail.com>
    Signed-off-by: Zahir Hussain <zahir.basha@kpit.com>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>
    (cherry picked from commit 5aeada5793af53e8c93940952d4f314474dca4c2)
    Signed-off-by: Steve Sakoman <steve@sakoman.com>

--- meta/recipes-devtools/cmake/cmake/OEToolchainConfig.cmake
+++ meta/recipes-devtools/cmake/cmake/OEToolchainConfig.cmake
@@ -19,3 +19,6 @@ file( GLOB toolchain_config_files "${CMAKE_TOOLCHAIN_FILE}.d/*.cmake" )
 foreach(config ${toolchain_config_files})
     include(${config})
 endforeach()
+
+unset(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES)
+unset(CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES)
