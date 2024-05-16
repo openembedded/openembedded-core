c7830c5879f6fa68fa9f47ee59b7bf7f2d276c81
Philip Lorenz <philip.lorenz@bmw.de>
2024-05-16 09:24:38 +0200

    package_manager: Move OpkgDpkgPM into common module
    The OpkgDpkgPM class was introduced to share common functionality
    between the Opkg and Debian package manager implementations. However,
    for unknown reasons , the refactoring done in
    5bc67f55028407de78ac09f97f9a47b165ae8760 duplicated the common class
    into the deb and ipk modules. Undo this part of the change by moving the
    common base class into a newly created module.
    
    The two variants did not diverge a lot (next to the payload name
    generalization, the Debian variant missed
    17e2eaed036e1da8e7cb42cb3de51b9523ba54ec) and as such no regressions
    should be expected.
    
    Signed-off-by: Philip Lorenz <philip.lorenz@bmw.de>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oe/package_manager/ipk/__init__.py
+++ meta/lib/oe/package_manager/ipk/__init__.py
@@ -4,11 +4,11 @@
 # SPDX-License-Identifier: GPL-2.0-only
 #
 
-import glob
 import re
 import shutil
 import subprocess
 from oe.package_manager import *
+from oe.package_manager.common_deb_ipk import OpkgDpkgPM
 
 class OpkgIndexer(Indexer):
     def write_index(self):
@@ -90,82 +90,6 @@ class PMPkgsList(PkgsList):
 
         return opkg_query(cmd_output)
 
-
-
-class OpkgDpkgPM(PackageManager):
-    def __init__(self, d, target_rootfs):
-        """
-        This is an abstract class. Do not instantiate this directly.
-        """
-        super(OpkgDpkgPM, self).__init__(d, target_rootfs)
-
-    def package_info(self, pkg, cmd):
-        """
-        Returns a dictionary with the package info.
-
-        This method extracts the common parts for Opkg and Dpkg
-        """
-
-        proc = subprocess.run(cmd, capture_output=True, encoding="utf-8", shell=True)
-        if proc.returncode:
-            bb.fatal("Unable to list available packages. Command '%s' "
-                     "returned %d:\n%s" % (cmd, proc.returncode, proc.stderr))
-        elif proc.stderr:
-            bb.note("Command '%s' returned stderr: %s" % (cmd, proc.stderr))
-
-        return opkg_query(proc.stdout)
-
-    def extract(self, pkg, pkg_info):
-        """
-        Returns the path to a tmpdir where resides the contents of a package.
-
-        Deleting the tmpdir is responsability of the caller.
-
-        This method extracts the common parts for Opkg and Dpkg
-        """
-
-        ar_cmd = bb.utils.which(os.getenv("PATH"), "ar")
-        tar_cmd = bb.utils.which(os.getenv("PATH"), "tar")
-        pkg_path = pkg_info[pkg]["filepath"]
-
-        if not os.path.isfile(pkg_path):
-            bb.fatal("Unable to extract package for '%s'."
-                     "File %s doesn't exists" % (pkg, pkg_path))
-
-        tmp_dir = tempfile.mkdtemp()
-        current_dir = os.getcwd()
-        os.chdir(tmp_dir)
-
-        try:
-            cmd = [ar_cmd, 'x', pkg_path]
-            output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-            data_tar = glob.glob("data.tar.*")
-            if len(data_tar) != 1:
-                bb.fatal("Unable to extract %s package. Failed to identify "
-                         "data tarball (found tarballs '%s').",
-                         pkg_path, data_tar)
-            data_tar = data_tar[0]
-            cmd = [tar_cmd, 'xf', data_tar]
-            output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError as e:
-            bb.utils.remove(tmp_dir, recurse=True)
-            bb.fatal("Unable to extract %s package. Command '%s' "
-                     "returned %d:\n%s" % (pkg_path, ' '.join(cmd), e.returncode, e.output.decode("utf-8")))
-        except OSError as e:
-            bb.utils.remove(tmp_dir, recurse=True)
-            bb.fatal("Unable to extract %s package. Command '%s' "
-                     "returned %d:\n%s at %s" % (pkg_path, ' '.join(cmd), e.errno, e.strerror, e.filename))
-
-        bb.note("Extracted %s to %s" % (pkg_path, tmp_dir))
-        bb.utils.remove(os.path.join(tmp_dir, "debian-binary"))
-        bb.utils.remove(os.path.join(tmp_dir, "control.tar.gz"))
-        bb.utils.remove(os.path.join(tmp_dir, data_tar))
-        os.chdir(current_dir)
-
-        return tmp_dir
-
-    def _handle_intercept_failure(self, registered_pkgs):
-        self.mark_packages("unpacked", registered_pkgs.split())
 
 class OpkgPM(OpkgDpkgPM):
     def __init__(self, d, target_rootfs, config_file, archs, task_name='target', ipk_repo_workdir="oe-rootfs-repo", filterbydependencies=True, prepare_index=True):
