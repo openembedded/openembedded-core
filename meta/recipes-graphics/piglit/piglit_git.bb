openembedded/openembedded-core

commit 9662bdabf3f638416963d1a8bab584680f4304a0

piglit: upgrade 22eaf6a91c -> c11c9374c1
Commit 766669895d fixes builds with numpy 2.x.

Patch '0001-utils-Include-libgen.h-on-musl-linux-systems.patch' dropped
because it is now upstream in commit 9c941dfc36.

Add wayland-native and wayland-protocols to DEPENDS to avoid errors
during build.

There's a reproducibility issue being seen on the autobuilder. It
appears that it's a host contamination issue, based on what Richard
replied with in the v3. See: https://lists.openembedded.org/g/openembedded-core/message/203651

Does anyone with more CMake familiarity know how to patch CMakeLists.txt
or the recipe in a way that will fix these errors? I'm not able to see
any of them locally. It's also possible that what I've added to
PACKAGECONFIG is not the right approach for x11 support.

Changelog:

c11c9374c (HEAD -> main, origin/main, origin/HEAD) arb_bindless_texture: test image/sampler constructors
f11abb664 Revert "arb_bindless_texture: add another function param bindless test"
045343687 arb_gpu_shader5: add linker version of function overloads test
4b1116f3a arb_bindless_texture: add another function param bindless test
4a62c2672 glsl: test function linking with implicit param conversions
9f280db77 arb_bindless_texture: add basic function param bindless tests
766669895 generated_tests: fix numpy overflow warnings that are errors in numpy 2.0
5214915dc generated_tests: print mako exceptions
41bd119f2 arb_texture_buffer_object: don't leak src strings
582f5490a gl-1.0: Add a test for glPushAttrib/glPopAttrib
647d07250 ext_texture_format_bgra8888: test GL_BGRA8_EXT as well
dee2a233f perf: port glsl-compile-time perf-test from mesa demos
18333c568 perf: port vertexrate perf-test from mesa demos
e932fa2f5 perf: port vbo perf-test from mesa demos
f9cf4788a perf: port teximage perf-test from mesa demos
20ccc1c10 perf: port readpixels perf-test from mesa demos
556a9c96d perf: port genmipmap perf-test from mesa demos
225b6bfb6 perf: port fill perf-test from mesa demos

meta/recipes-graphics/piglit/piglit_git.bb
@@ -10,11 +10,10 @@ SRC_URI = "git://gitlab.freedesktop.org/mesa/piglit.git;protocol=https;branch=ma
            file://0002-cmake-use-proper-WAYLAND_INCLUDE_DIRS-variable.patch \
            file://0003-tests-util-piglit-shader.c-do-not-hardcode-build-pat.patch \
            file://0001-tests-Fix-narrowing-errors-seen-with-clang.patch \
-           file://0001-utils-Include-libgen.h-on-musl-linux-systems.patch \
            "
 UPSTREAM_CHECK_COMMITS = "1"
 
-SRCREV = "22eaf6a91cfd57f7bb3df4e5068c2ac1472d4ec1"
+SRCREV = "c11c9374c1448d01b31e7f724921cc1f4ee2c84c"
 # (when PV goes above 1.0 remove the trailing r)
 PV = "1.0+gitr"
 
@@ -23,7 +22,7 @@ S = "${WORKDIR}/git"
 X11_DEPS = "${@bb.utils.contains('DISTRO_FEATURES', 'x11', 'virtual/libx11 libxrender libglu', '', d)}"
 X11_RDEPS = "${@bb.utils.contains('DISTRO_FEATURES', 'x11', 'mesa-demos', '', d)}"
 
-DEPENDS = "libpng waffle libxkbcommon python3-mako-native python3-numpy-native python3-six-native virtual/egl"
+DEPENDS = "libpng waffle libxkbcommon python3-mako-native python3-numpy-native python3-six-native virtual/egl wayland-native wayland-protocols"
 
 inherit cmake pkgconfig python3native features_check bash-completion
 
@@ -40,7 +39,7 @@ PACKAGECONFIG ??= "${@bb.utils.contains('DISTRO_FEATURES', 'x11', 'x11 glx', '',
 PACKAGECONFIG[freeglut] = "-DPIGLIT_USE_GLUT=1,-DPIGLIT_USE_GLUT=0,freeglut,"
 PACKAGECONFIG[glx] = "-DPIGLIT_BUILD_GLX_TESTS=ON,-DPIGLIT_BUILD_GLX_TESTS=OFF"
 PACKAGECONFIG[opencl] = "-DPIGLIT_BUILD_CL_TESTS=ON,-DPIGLIT_BUILD_CL_TESTS=OFF,virtual/opencl-icd"
-PACKAGECONFIG[x11] = "-DPIGLIT_BUILD_GL_TESTS=ON,-DPIGLIT_BUILD_GL_TESTS=OFF,${X11_DEPS}, ${X11_RDEPS}"
+PACKAGECONFIG[x11] = "-DPIGLIT_USE_X11=1 -DPIGLIT_BUILD_GL_TESTS=ON -DPIGLIT_BUILD_DMA_BUF_TESTS=ON,-DPIGLIT_USE_X11=0 -DPIGLIT_BUILD_GL_TESTS=OFF -DPIGLIT_BUILD_DMA_BUF_TESTS=OFF,${X11_DEPS}, ${X11_RDEPS}"
 PACKAGECONFIG[vulkan] = "-DPIGLIT_BUILD_VK_TESTS=ON,-DPIGLIT_BUILD_VK_TESTS=OFF,glslang-native vulkan-loader,glslang"
 
 export PIGLIT_BUILD_DIR = "../../../../git"

meta/recipes-graphics/piglit/piglit/0001-utils-Include-libgen.h-on-musl-linux-systems.patch
@@ -1,83 +1,0 @@
-From 6c852e6ac292008137a6f3a8aa908090bb5b4b11 Mon Sep 17 00:00:00 2001
-From: Erik Faye-Lund <erik.faye-lund@collabora.com>
-Date: Tue, 19 Mar 2024 16:34:02 +0100
-Subject: [PATCH] properly check for libgen.h
-
-Some users are reporting that basename is not available when building on
-Linux with musl libc. And since the POSIX spec[1] says that basename is
-defined in libgen.h, we should include that when available.
-
-So let's properly detect the header, and include it if it exists. This
-should hopefully make things a bit more robust.
-
-Since we're also including this from the CL program-tester, let's
-rearrange the includes a bit so we know that config.h has been included.
-
-Upstream-Status: Submitted [https://gitlab.freedesktop.org/mesa/piglit/-/merge_requests/888]
-Reviewed-by: David Heidelberg <david.heidelberg@collabora.com>
-Signed-off-by: Khem Raj <raj.khem@gmail.com>
----
- CMakeLists.txt                    | 1 +
- tests/cl/program/program-tester.c | 5 ++++-
- tests/util/config.h.in            | 1 +
- tests/util/piglit-util.h          | 4 ++--
- 4 files changed, 8 insertions(+), 3 deletions(-)
-
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index dd2bf67125..5563fe0e20 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -492,6 +492,7 @@ check_include_file(unistd.h    HAVE_UNISTD_H)
- check_include_file(fcntl.h     HAVE_FCNTL_H)
- check_include_file(linux/sync_file.h HAVE_LINUX_SYNC_FILE_H)
- check_include_file(endian.h    HAVE_ENDIAN_H)
-+check_include_file(libgen.h    HAVE_LIBGEN_H)
- 
- if(DEFINED PIGLIT_INSTALL_VERSION)
- 	set(PIGLIT_INSTALL_VERSION_SUFFIX
-diff --git a/tests/cl/program/program-tester.c b/tests/cl/program/program-tester.c
-index 97fe64906d..e47fb5aacc 100644
---- a/tests/cl/program/program-tester.c
-+++ b/tests/cl/program/program-tester.c
-@@ -31,10 +31,13 @@
- #include <inttypes.h>
- #include <math.h>
- #include <regex.h>
--#include <libgen.h>
- 
- #include "piglit-framework-cl-program.h"
- 
-+#ifdef HAVE_LIBGEN_H
-+#include <libgen.h>
-+#endif
-+
- /* Regexes */
- 
- /*
-diff --git a/tests/util/config.h.in b/tests/util/config.h.in
-index 8ed5af1709..437eb91418 100644
---- a/tests/util/config.h.in
-+++ b/tests/util/config.h.in
-@@ -16,3 +16,4 @@
- #cmakedefine HAVE_SYS_RESOURCE_H 1
- #cmakedefine HAVE_UNISTD_H 1
- #cmakedefine HAVE_ENDIAN_H 1
-+#cmakedefine HAVE_LIBGEN_H 1
-diff --git a/tests/util/piglit-util.h b/tests/util/piglit-util.h
-index 4d3606c708..de999980b6 100644
---- a/tests/util/piglit-util.h
-+++ b/tests/util/piglit-util.h
-@@ -52,8 +52,8 @@ extern "C" {
- #include <math.h>
- #include <float.h>
- 
--#if defined(__APPLE__) || defined(__MINGW32__)
--#  include "libgen.h" // for basename
-+#ifdef HAVE_LIBGEN_H
-+#  include <libgen.h> // for basename
- #elif defined(_MSC_VER)
- 
- static inline char *
--- 
-GitLab
-

commit 6c504d03c8bd8e2a9c8d0b946dbf28e94d13b5d6

piglit: add missing dependency on wayland
After the recent piglit update it fails X11 builds reporting that it can
not find wayland-client package. Commit 9662bdabf3f6 ("piglit: upgrade
22eaf6a91c -> c11c9374c1") has added wayland-related dependencies to the
recipe's DEPENDS variable, but it seems it missed the target wayland
pacakge, making cross-compilation of piglit fail when building
X11-targeted configurations.

Pushing more and more wayland dependencies to a generic build doesn't
seem to be a good idea, so introduce "wayland" PACKAGECONFIG option,
move existing wayland-related dependencies to be guarded by it and add
dependency on the wayland package.

Suggested-by: Alexander Kanavin <alex.kanavin@gmail.com>
Fixes: 9662bdabf3f6 ("piglit: upgrade 22eaf6a91c -> c11c9374c1")
Cc: Trevor Gamblin <tgamblin@baylibre.com>
Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

meta/recipes-graphics/piglit/piglit_git.bb
@@ -23,7 +23,7 @@ S = "${WORKDIR}/git"
 X11_DEPS = "${@bb.utils.contains('DISTRO_FEATURES', 'x11', 'virtual/libx11 libxrender libglu', '', d)}"
 X11_RDEPS = "${@bb.utils.contains('DISTRO_FEATURES', 'x11', 'mesa-demos', '', d)}"
 
-DEPENDS = "libpng waffle libxkbcommon python3-mako-native python3-numpy-native python3-six-native virtual/egl wayland-native wayland-protocols"
+DEPENDS = "libpng waffle libxkbcommon python3-mako-native python3-numpy-native python3-six-native virtual/egl"
 
 inherit cmake pkgconfig python3native features_check bash-completion
 
@@ -36,12 +36,16 @@ REQUIRED_DISTRO_FEATURES += "opengl"
 export TEMP = "${B}/temp/"
 do_compile[dirs] =+ "${B}/temp/"
 
-PACKAGECONFIG ??= "${@bb.utils.contains('DISTRO_FEATURES', 'x11', 'x11 glx', '', d)}"
+PACKAGECONFIG ??= " \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'x11', 'x11 glx', '', d)} \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'wayland', 'wayland', '', d)} \
+"
 PACKAGECONFIG[freeglut] = "-DPIGLIT_USE_GLUT=1,-DPIGLIT_USE_GLUT=0,freeglut,"
 PACKAGECONFIG[glx] = "-DPIGLIT_BUILD_GLX_TESTS=ON,-DPIGLIT_BUILD_GLX_TESTS=OFF"
 PACKAGECONFIG[opencl] = "-DPIGLIT_BUILD_CL_TESTS=ON,-DPIGLIT_BUILD_CL_TESTS=OFF,virtual/opencl-icd"
 PACKAGECONFIG[x11] = "-DPIGLIT_USE_X11=1 -DPIGLIT_BUILD_GL_TESTS=ON -DPIGLIT_BUILD_DMA_BUF_TESTS=ON,-DPIGLIT_USE_X11=0 -DPIGLIT_BUILD_GL_TESTS=OFF -DPIGLIT_BUILD_DMA_BUF_TESTS=OFF,${X11_DEPS}, ${X11_RDEPS}"
 PACKAGECONFIG[vulkan] = "-DPIGLIT_BUILD_VK_TESTS=ON,-DPIGLIT_BUILD_VK_TESTS=OFF,glslang-native vulkan-loader,glslang"
+PACKAGECONFIG[wayland] = "-DPIGLIT_USE_WAYLAND=1,-DPIGLIT_USE_WAYLAND=0,wayland-native wayland wayland-protocols"
 
 export PIGLIT_BUILD_DIR = "../../../../git"
 