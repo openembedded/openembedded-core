8c6ad2efe5222a730fec69c2d7488e7e414e6de9
Jeroen Hofstee <jhofstee@victronenergy.com>
2025-04-07 19:49:04 +0200

    udev-extraconf: fix ifupdown for non hotplug devices
    Commit 160f713917 (udev-extraconf: fix network.sh script did not
    configure hotplugged interfaces, 2024-10-18) fixed ifupdown for
    hotplug devices, but also calls it for non hotplug devices. That
    can cause issue, since they might not expect the ifupdown from
    udev, since it wasn't called before mentioned patch got merged.
    
    For util-linux this can simply be fixed by adding --allow=hotplug.
    Unfortunately busybox doesn't have that option, so a function is
    added to check if the device is marked as allow-hotplug.
    
    Since wilcards are supported adding 'allow-hotplug *' allows to
    restore behaviour of mentioned patch, while this restores the
    original behaviour.
    
    Signed-off-by: Jeroen Hofstee <jhofstee@victronenergy.com>
    Signed-off-by: Mathieu Dubois-Briand <mathieu.dubois-briand@bootlin.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/recipes-core/udev/udev-extraconf/network.sh
+++ meta/recipes-core/udev/udev-extraconf/network.sh
@@ -6,6 +6,34 @@ echo "$INTERFACE" | grep -q wifi && exit 0
 # udevd does clearenv(). Export shell PATH to children.
 export PATH
 
+# udev should only trigger ifupdown for interfaces marked as allow-hotplug
+# and with util-linux that is as simple as adding --allow=hotplug.
+# Busybox unfortunately doesn't have this option.
+# allow-hotplug is a pattern like eth0 /eth* /eth*/1 /eth*=eth.
+# This function checks if INTERFACE matches an allow-hotplug pattern.
+
+allow_hotplug() {
+    allow_hotplug="$(sed -n -e 's/^allow-hotplug \+\([^= ]*\).*/\1/p' /etc/network/interfaces)"
+    for pattern in $allow_hotplug; do
+        options="$(echo $pattern | sed -n -e 's,^/\?[^ /]\+/\(.*\),\1,p')"
+        value="$(echo $pattern | sed -n -e 's,^/\?\([^ /]\+\).*,\1,p')"
+        interfaces="$(ls -d /sys/class/net/$value 2>/dev/null | xargs -r -n 1 basename)"
+        if [ "$options" != "" ]; then
+            interfaces="$(echo $interfaces | awk -v n=$options '{print $n }')"
+        fi
+        echo "$interfaces" | grep -w -q "$INTERFACE"
+        if [ $? -eq 0 ]; then
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+if ! allow_hotplug; then
+    exit 0
+fi
+
 # if this interface has an entry in /etc/network/interfaces, let ifupdown
 # handle it
 if grep -q "iface \+$INTERFACE" /etc/network/interfaces; then
