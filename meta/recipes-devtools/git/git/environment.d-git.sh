183e043de423fd3f7833366ca524a6f7d17e6d14
Changqing Li <changqing.li@windriver.com>
2025-04-08 17:38:22 +0800

    buildtools-tarball: add envvars into BB_ENV_PASSTHROUGH_ADDITIONS
    Here is one testcase:
    For recipe tensorflow-lite-host-tools_2.18.0.bb, refer [1],
    do_configure[network] = "1"
    and it will git clone some repos in CMakeLists.txt
    
    When buildtools is used and nativesdk-git is installed into sdk,
    do_configure failed with error:
    [1/9] Performing download step (git clone) for 'protobuf-populate'
    Cloning into 'protobuf'...
    fatal: unable to access 'https://github.com/protocolbuffers/protobuf/': error setting certificate file: /usr/local/oe-sdk-hardcoded-buildpath/sysroots/x86_64-wrlinuxsdk-linux/etc/ssl/certs/ca-certificates.crt
    
    Fix by adding GIT_SSL_CAINFO in BB_ENV_PASSTHROUGH_ADDITIONS, so that
    user can export GIT_SSL_CAINFO=${GIT_SSL_CAINFO} in their
    do_configure:prepend() to fix above do_configure failure
    
    CURL_CA_BUNDLE and REQUESTS_CA_BUNDLE is similar envvars, so all add
    into BB_ENV_PASSTHROUGH_ADDITIONS
    
    [1] https://github.com/nxp-imx/meta-imx/blob/styhead-6.12.3-1.0.0/meta-imx-ml/recipes-libraries/tensorflow-lite/tensorflow-lite-host-tools_2.18.0.bb
    
    Signed-off-by: Changqing Li <changqing.li@windriver.com>
    Signed-off-by: Mathieu Dubois-Briand <mathieu.dubois-briand@bootlin.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/recipes-devtools/git/git/environment.d-git.sh
+++ meta/recipes-devtools/git/git/environment.d-git.sh
@@ -1,3 +1,4 @@
 if [ -e "${OECORE_NATIVE_SYSROOT}/etc/ssl/certs/ca-certificates.crt" ];then
     export GIT_SSL_CAINFO="${OECORE_NATIVE_SYSROOT}/etc/ssl/certs/ca-certificates.crt"
+    export BB_ENV_PASSTHROUGH_ADDITIONS="${BB_ENV_PASSTHROUGH_ADDITIONS:-} GIT_SSL_CAINFO"
 fi
