b0ce480eca6397fab71082ed202c3cf9dd02456f
Steve Sakoman <steve@sakoman.com>
2025-10-08 14:23:36 -0700

    oeqa/sdk/cases/buildcpio.py: use gnu mirror instead of main server
    ftp.gnu.org is the main server of the GNU project, however download speed
    can vary greatly based on one's location.
    
    Using ftpmirror.gnu.org should redirect the request to the closest up-to-date mirror,
    which should result sometimes in significantly faster download speed, depending
    on one's location. This should also distribute the traffic more across the mirrors.
    
    This information was sourced from https://www.gnu.org/prep/ftp.html
    
    Signed-off-by: Steve Sakoman <steve@sakoman.com>

--- meta/lib/oeqa/sdk/cases/buildcpio.py
+++ meta/lib/oeqa/sdk/cases/buildcpio.py
@@ -24,7 +24,7 @@ class BuildCpioTest(OESDKTestCase):
 
     def test_cpio(self):
         with tempfile.TemporaryDirectory(prefix="cpio-", dir=self.tc.sdk_dir) as testdir:
-            tarball = self.fetch(testdir, self.td["DL_DIR"], "https://ftp.gnu.org/gnu/cpio/cpio-2.15.tar.gz")
+            tarball = self.fetch(testdir, self.td["DL_DIR"], "https://ftpmirror.gnu.org/gnu/cpio/cpio-2.15.tar.gz")
 
             dirs = {}
             dirs["source"] = os.path.join(testdir, "cpio-2.15")
