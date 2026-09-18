4171f85bbc60cd8cbb95b768438b6b670cd276e0
Richard Purdie <richard.purdie@linuxfoundation.org>
2026-09-18 18:28:14 +0100

    lib/oe/__init__: Fix typo
    This should reference visitorcode in qa.py (spoted by Peter Kjellerstedt,
    thanks).
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oe/__init__.py
+++ meta/lib/oe/__init__.py
@@ -7,7 +7,7 @@
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
 
-# Modules with vistorcode need to go first else anything depending on them won't be
+# Modules with visitorcode need to go first else anything depending on them won't be
 # processed correctly (e.g. qa)
 BBIMPORTS = ["qa", "data", "path", "utils", "types", "package", "packagedata", \
              "packagegroup", "sstatesig", "lsb", "cachedpath", "license", "qemu", \
